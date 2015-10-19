cfae9e8a4603858b062ed9c6adc8c0c34602d463
Eric Dennison <ericd@netdenizen.com>
2015-10-19 13:38:38 -0400

    Update spacewar.py

--- spacewar.py
+++ spacewar.py
@@ -4,6 +4,6 @@ Author: <your name here>
 Credit: <list sources used, if any>
 
 Assignment:
-Write and submit a program that implements the sandbox platformer game:
+Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
