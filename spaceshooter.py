2690efe73a81e3a71bf2f36907208cbdabc6cff2
voidJeff <qy120838@126.com>
2015-10-20 14:29:32 -0400

    Updated from Brython Server: 10/20/2015 2:29:32 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -32,8 +32,8 @@ class SpaceShip(Sprite):
         self.fxcenter = self.fycenter = 0.5
         
     def step(self):
-        self.x += self.vx
-        self.y += self.vy
+        self.x -= self.vx
+        self.y -= self.vy
         self.rotation += self.vr
         if self.thrust == 1:
             self.setImage(self.thrustframe)
