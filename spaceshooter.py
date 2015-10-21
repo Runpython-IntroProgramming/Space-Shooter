db49fc9525a1e2b0798d51237f69f964ddc01bee
ChubbyPotato <suhangui@hanovernorwichschools.org>
2015-10-21 13:26:53 -0400

    Updated from Brython Server: 10/21/2015 1:26:53 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,9 +1,9 @@
 """
 spaceshooter.py
 Author: Suhan Gui
-Credit: 
-
+Credit:
 Assignment:
 Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
