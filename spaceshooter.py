ecedc57fed4caf4c99254030cf85860fc3ef8548
ChubbyPotato <suhangui@hanovernorwichschools.org>
2015-10-23 14:13:18 -0400

    Updated from Brython Server: 10/23/2015 2:13:17 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -16,9 +16,28 @@ SCREEN_HEIGHT = 480
 
 class Ship(Sprite):
     image=ImageAsset("images/four_spaceship_by_albertov.png")
+    width=100
+    height=100
+    
+
 class Suns(Sprite):
     image=ImageAsset("images/sun.png")
-class Moving(Sprite)
+    width=100
+    height=100
+
+class Moving(Sprite):
+    image=ImageAsset("images/four_spaceship_by_albertov_with_thrust.png")
+    width=100
+    height=100
+    
+
+class Galaxy(Sprite):
+    image=ImageAsset("images/starfield.jpg")
+    width=SCREEN_WIDTH
+    height=SCREEN_HEIGHT
+    
+class SpaceGame(App):
+    
 
 #Fin
 myapp = SpaceGame(SCREEN_WIDTH, SCREEN_HEIGHT)
