dd621c000a69b3d3edfc7f498f99e7ff222ea93f
ChubbyPotato <suhangui@hanovernorwichschools.org>
2015-10-27 14:17:13 -0400

    Updated from Brython Server: 10/27/2015 2:17:13 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -12,8 +12,8 @@ class Ship(Sprite):
     image=ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", Frame(100,0,225,125), 4, 'vertical')
     def __init__(self, position):
         super().__init__(Ship.image, position)
-        self.vx=1
-        self.vy=1
+        self.vx=0
+        self.vy=0
         self.vr=0
         self.trust=0
         self.trustframe=0
