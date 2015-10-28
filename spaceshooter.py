43d71c00b87c55fce8e24ecac8bcc2c0d139d800
nilskingston <nilskingston@hanovernorwichschools.org>
2015-10-28 13:50:02 -0400

    Updated from Brython Server: 10/28/2015 1:50:03 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -22,10 +22,16 @@ bg = Sprite(bg_asset, (0,0))
 class SpaceShip(Sprite):
     asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
         Frame(227,0,292-227,125), 4, 'vertical')
-
     def __init__(self, position):
         super().__init__(SpaceShip.asset, position)
-
+        self.vx = 1
+        self.vy = 1
+        self.vr = 0.01
+        
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+        self.rotation += self.vr
 
 class SpaceGame(App):
     def __init__(self, width, height):
@@ -35,6 +41,7 @@ class SpaceGame(App):
         bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
         bg = Sprite(bg_asset, (0,0))
         SpaceShip((100,100))
-
+        SpaceShip((150,150))
+        SpaceShip((200,50))
 myapp = SpaceGame(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
