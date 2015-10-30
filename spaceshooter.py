69dc8273e99572bb7094bd74e856617fbf47a277
ChubbyPotato <suhangui@hanovernorwichschools.org>
2015-10-30 08:57:44 -0400

    Updated from Brython Server: 10/30/2015 8:57:43 AM

--- spaceshooter.py
+++ spaceshooter.py
@@ -63,6 +63,62 @@ class Ship(Sprite):
         
     def NoturnRight(self,event):
         self,vr=0
+        
+class Ship(Sprite):
+    image=ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", Frame(227,0,292-227,125), 4, 'vertical')
+    def __init__(self, position):
+        super().__init__(Ship.image, position)
+        self.vx=0
+        self.vy=0
+        self.vr=0
+        self.thrust=0
+        self.thrustframe=0
+        SpaceGame.listenKeyEvent("keydown","s", self.moveBackward)
+        SpaceGame.listenKeyEvent("keyup","s", self.NomoveBackward)
+        SpaceGame.listenKeyEvent("keydown","w", self.moveForward)
+        SpaceGame.listenKeyEvent("keyup","w", self.NomoveForward)
+        SpaceGame.listenKeyEvent("keydown","a",self.turnLeft)
+        SpaceGame.listenKeyEvent("keyup","a",self.NoturnLeft)
+        SpaceGame.listenKeyEvent("keydown","d", self.turnRight)
+        SpaceGame.listenKeyEvent("keyup","d", self.NoturnRight)
+        self.fxcenter = 0.5
+        self.fycenter = 0.5
+
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+        self.rotation += self.vr
+        if self.thrust == 1:
+            self.setImage(self.thrust)
+            self.thrust += 1
+            if self.thrust == 4:
+                self.thrust = 1
+        else:
+            self.setImage(0)
+
+    def moveFoward(self, event):
+        self.thrust = -1
+        
+    def NomoveForward(self, event):
+        self.thrust=0
+
+    def moveBackward(self, event):
+        self.thrust = 1
+        
+    def NomoveBackward(self,event):
+        self.thrust=0
+
+    def turnLeft(self,event):
+        self.vr=1
+
+    def NoturnLeft(self,event):
+        self.vr=0
+
+    def turnRight(self, event):
+        self.vr=-1
+        
+    def NoturnRight(self,event):
+        self,vr=0
 
     
 """
