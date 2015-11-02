8794b50275c9186ccae71b9e1b8513274dbcd2e5
APikielny <apikielny@gmail.com>
2015-11-02 14:57:46 -0500

    Updated from Brython Server: November 2, 2015 2:57:46 PM EST

--- spaceshooter.py
+++ spaceshooter.py
@@ -20,7 +20,7 @@ class SpaceShip(Sprite):
     asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
         Frame(227,0,292-227,125), 4, 'vertical')
 
-    def __init__(self, position):
+    def __init__(self, position, app):
         super().__init__(SpaceShip.asset, position)
         self.vx = .0001
         self.vy = .0001
@@ -45,7 +45,7 @@ class SpaceShip(Sprite):
         #is the below function in the write step?
         #if collidingWithSprites(self)==True:
             #print("collision")
-        if self.collidingWith(sun)==True:
+        if self.collidingWith(app.sun)==True:
             self.stop
         if self.thrust == 1:
             self.setImage(self.thrustframe)
@@ -89,8 +89,8 @@ class SpaceGame(App):
         bg_asset = ImageAsset("images/starfield.jpg")
         bg=Sprite(bg_asset,(0,0))
         sun_asset = ImageAsset("images/sun.png")
-        sun=Sprite(sun_asset, (200,200))
-        SpaceShip((100,100))
+        self.sun=Sprite(sun_asset, (200,200))
+        SpaceShip((100,100), self)
         #SpaceShip((150,150))
         #SpaceShip((200,50))
 
