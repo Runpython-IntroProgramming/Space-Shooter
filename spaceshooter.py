e0f3a64ec2502eb272013e72a603efff7fcc5870
RDanilek <rogerdanilek@hanovernorwichschools.org>
2015-11-03 14:26:02 -0500

    Updated from Brython Server: November 3, 2015 2:26:04 PM GMT-5

--- spaceshooter.py
+++ spaceshooter.py
@@ -63,10 +63,10 @@ class SpaceShip(Sprite):
         self.vx+=(.5*(math.sin(self.rotation)))
         
     def turnright(self, event):
-        self.vr-=
+        self.vr-=.005
 
     def turnleft(self, event):
-        self.vr+=1
+        self.vr+=.005
 
 
 
