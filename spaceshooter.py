6de1b29bdc5fb8304b07b53046acb66aed8f5b23
RDanilek <rogerdanilek@hanovernorwichschools.org>
2015-11-06 14:27:11 -0500

    Updated from Brython Server: November 6, 2015 2:27:14 PM GMT-5

--- spaceshooter.py
+++ spaceshooter.py
@@ -8,6 +8,7 @@ Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+import math 
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
