ea1834d8928f888498b90ff89d3bcae0ac242c4d
RDanilek <rogerdanilek@hanovernorwichschools.org>
2015-11-10 19:41:46 -0500

    Updated from Brython Server: November 10, 2015 7:41:49 PM GMT-5

--- spaceshooter.py
+++ spaceshooter.py
@@ -19,7 +19,7 @@ class SpaceShip(Sprite):
     Animated space ship
     """
     asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
-        Frame(227,0,292-227,125), 4, 'vertical')
+        Frame(151,0,292-227,125), 4, 'vertical')
 
     def __init__(self, position, s):
         super().__init__(SpaceShip.asset, position)
