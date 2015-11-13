5dae5e6682a6c269a8a7be82fa6a8deecbeea140
ryankynor <ryankynor@hanovernorwichschools.org>
2015-11-13 14:25:38 -0500

    Updated from Brython Server: 11/13/2015 2:25:39 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,9 +1,25 @@
 """
 spaceshooter.py
-Author: <your name here>
+Author: Ryan Kynor
 Credit: <list sources used, if any>
 
 Assignment:
 Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
+
+
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+# Background
+black = Color(0, 1)
+noline = LineStyle(0, black)
+bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
+bg = Sprite(bg_asset, (0,0))
+
+
+myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
+myapp.run()
