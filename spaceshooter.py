86c3bb752f20ace4ad24b71a926b16c6912c7849
ryankynor <ryankynor@hanovernorwichschools.org>
2015-11-16 14:36:21 -0500

    Updated from Brython Server: 11/16/2015 2:36:22 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,14 +1,7 @@
 """
-spaceshooter.py
-Author: Ryan Kynor
-Credit: <list sources used, if any>
-
-Assignment:
-Write and submit a program that implements the spacewar game:
-https://github.com/HHS-IntroProgramming/Spacewar
+Space game
+Ryan Kynor
 """
-
-
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
 
 SCREEN_WIDTH = 640
@@ -24,10 +17,34 @@ class SpaceShip(Sprite):
 
     def __init__(self, position):
         super().__init__(SpaceShip.asset, position)
+        self.vx = 1
+        self.vy = 1
+        self.vr = 0.01
         self.thrust = 0
         self.thrustframe = 1
         SpaceGame.listenKeyEvent("keydown", "space", self.thrustOn)
         SpaceGame.listenKeyEvent("keyup", "space", self.thrustOff)
+        self.fxcenter = self.fycenter = 0.5
+
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+        self.rotation += self.vr
+        if self.thrust == 1:
+            self.setImage(self.thrustframe)
+            self.thrustframe += 1
+            if self.thrustframe == 4:
+                self.thrustframe = 1
+        else:
+            self.setImage(0)
+
+    def thrustOn(self, event):
+        self.thrust = 1
+
+    def thrustOff(self, event):
+        self.thrust = 0
+
+
 
 class SpaceGame(App):
     """
@@ -35,32 +52,18 @@ class SpaceGame(App):
     """
     def __init__(self, width, height):
         super().__init__(width, height)
-        #Backround
         black = Color(0, 1)
         noline = LineStyle(0, black)
-        bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
+        bg_asset = RectangleAsset(width, height, noline, black)
         bg = Sprite(bg_asset, (0,0))
         SpaceShip((100,100))
         SpaceShip((150,150))
         SpaceShip((200,50))
-        self.vx = 1
-        self.vy = 1
-        self.vr = 0.01
-        
-def step(self):
+
+    def step(self):
         for ship in self.getSpritesbyClass(SpaceShip):
             ship.step()
-            def thrustOn(self, event):
-                self.thrust = 1
-                
-                
-def thrustOff(self, event):
-        self.thrust = 0
-def step(self):
-        self.x += self.vx
-        self.y += self.vy
-        self.rotation += self.vr
-        
+
 
 myapp = SpaceGame(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
