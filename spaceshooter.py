4fc46b8b3b12948000ae13ba30866204413577ce
CriticalD20 <jamesnapier@hanovernorwichschools.org>
2015-11-17 14:53:22 -0500

    Updated from Brython Server: 11/17/2015 2:53:22 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,7 +1,7 @@
 """
 spaceshooter.py
 Author: James Napier
-Credit: Space War Source Code
+Credit: Space War Source Code, Morgan
 
 Assignment:
 Write and submit a program that implements the spacewar game:
@@ -21,6 +21,8 @@ from time import time
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
+
+            
 class Stars(Sprite):
 
     asset = ImageAsset("images/starfield.jpg")
@@ -29,22 +31,24 @@ class Stars(Sprite):
 
     def __init__(self, position):
         super().__init__(Stars.asset, position)
-        
+
+            
 class gravitysprite(Sprite):
     #error in line above. Don't know what it is
-    def__init__(self, asset, position, velocity)
-    super().__init__(asset, position)
-    self.vx=velocity[0]
-    self.vy=velocity[1]
-    self.fxcenter=0.5
-    self.fycenter=0.5
-    self.rrate=0.0
-    self.thrust=0.0
-    self.mass=1.0
+    def __init__(self, asset, position, velocity):
+        super().__init__(position)
+    #insert asset, if it does not work
+        self.vx=velocity[0]
+        self.vy=velocity[1]
+        self.fxcenter=0.5
+        self.fycenter=0.5
+        self.rrate=0.0
+        self.thrust=0.0
+        self.mass=1.0
     #not sure how to translate this -->class GracitySprite(Sprite)<-- from the sourcecode 
 
 
-class Ship(gravitygprite):
+class Ship(gravitysprite):
     
     def registerKeys(self, leys):
         commands = ["left", "right", "forward"]
@@ -75,12 +79,12 @@ class Ship(gravitygprite):
             #still got some stuff for class(Ship)
         
 class Ship1(Ship):
-        asset = imageAsset("images/four_spaceship_by_albertiv_with_thrust.png",
+        asset = ImageAsset("images/four_spaceship_by_albertiv_with_thrust.png",
             Frame(227,0,292-227,125), 4, 'vertical')
             
-        def_init_(self, app, position, velocity)
-        super()._init_(Ship1.asset, app, position, velocity)
-        self.registerKeys(["a", "d", "w"])
+        def __init__(self, app, position, velocity):
+            super()._init_(Ship1.asset, app, position, velocity)
+            self.registerKeys(["a", "d", "w"])
             
         def step(self, t, dT):
             super().step(T, dT)
@@ -95,9 +99,9 @@ class Ship2(Ship):
         asset=ImageAsset("images/four_spaceship_by_albertov_with_thrust.png",
             Frame(0,0,86,125), 4, 'vertical')
             
-        def_init_(self, app, position, velocity)
-        super()._init_(Ship2.asset, app, position, velocity)
-        self.registerKeys(["left arrow", "right arrow", "up arrow"])
+        def __init__(self, app, position, velocity):
+            super()._init_(Ship2.asset, app, position, velocity)
+            self.registerKeys(["left arrow", "right arrow", "up arrow"])
             
         def step(self, T, dT):
             super().step(T, dT)
@@ -107,7 +111,42 @@ class Ship2(Ship):
                     if collides[0].visible:
                         collides[0].explode()
                         self.explode()
+class SpaceWar(App):
+    def __init__ (self, width, height):
+        super().__init__(width, height)
+        for x in range(self.width//Stars.width + 1):
+            for y in range(self.height//Stars.height + 1):
+                Stars((x*Stars.width, y*Stars.height))
+        self.ship1=Ship1(self,(self.width/2-140, self.height/2), (0, -120))
+        self.ship2=Ship2(self,(self.width/2+140, self.height/2), (0, 120))
+    #
+        self.tsprites = {k:Sprite(TextAsset(text=v, width=200, align='center',style='20px Arial', fill=Color(0xff2222,1))) 
+            for k, v in Spacewar.strings.items()}
+        self.listenKeyEvent('keydown', 'space', self.space)
+    #
+    def space(self, evt):
+        if self.state in ['instructions', 'gameover']:
+            for t in self.tsprites.values():
+                t.visible = False
+            self.state = 'playing'
+            self.Tlast = time()
+            evt.consumed = True
+            self.ship1.newgame()
+            self.ship2.newgame()
+            
+    def step(self):
+        explosions = self.getSpritesbyClass(ExplosionSmall)
+        for explosion in explosions:
+            explosion.step()
+        explosions = self.getSpritesbyClass(ExplosionBig)
+        for explosion in explosions:
+            explosion.step()
+        if self.state == 'instructions':
+            self.tsprites['space'].visible = True
+            self.tsprites['left'].visible = True
+            self.tsprites['right'].visible = True
                 
 
-
+app = SpaceWar(0,0)
+app.run()
 
