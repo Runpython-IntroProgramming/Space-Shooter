d022c2a26fbb1ab74e55e9e2d189c874c11e4cc9
SSupattapone <samuelsupattapone@hanovernorwichschools.org>
2015-11-23 14:54:53 -0500

    Updated from Brython Server: 11/23/2015 2:54:54 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -12,28 +12,29 @@ from ggame import SoundAsset, Sound, TextAsset, Color
 import math
 from time import time
 
-
-
-asset = ImageAsset("images/starfield.jpg")
+asset1 = ImageAsset("images/starfield.jpg")
 width = 512
 height = 512
 
-stars = Sprite(asset)
-stars1 = Sprite(asset)
+stars = Sprite(asset1)
+stars1 = Sprite(asset1)
 stars1.x = 512
 stars1.y = 0
-stars2 = Sprite(asset)
+stars2 = Sprite(asset1)
 stars2.x = 1024
 stars2.y = 0
-stars3 = Sprite(asset)
+stars3 = Sprite(asset1)
 stars3.x = 0
 stars3.y = 512
-stars4 = Sprite(asset)
+stars4 = Sprite(asset1)
 stars4.x = 512
 stars4.y = 512
-stars5 = Sprite(asset)
+stars5 = Sprite(asset1)
 stars5.x = 1024
 stars5.y = 512
+
+
+
 
 
 app = App()
