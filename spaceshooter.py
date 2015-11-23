4223060d3d85833f376ef2a991a3132613b7d32e
SSupattapone <samuelsupattapone@hanovernorwichschools.org>
2015-11-23 14:49:02 -0500

    Updated from Brython Server: 11/23/2015 2:49:03 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,9 +1,26 @@
 """
 spaceshooter.py
 Author: Sam Supattapone
-Credit: [none] as of now
+Credit: original code
 
 Assignment: Space Shooter
 Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
+from ggame import App, Sprite, ImageAsset, Frame
+from ggame import SoundAsset, Sound, TextAsset, Color
+import math
+from time import time
+
+
+
+asset = ImageAsset("images/starfield.jpg")
+width = 512
+height = 512
+
+stars = Sprite(asset)
+stars.width *= 2
+stars.height *= 2
+
+app = App()
+app.run()
