7cc3f7b24a343b32d2ed8c17ce5b9c74f5892bb7
ryankynor <ryankynor@hanovernorwichschools.org>
2015-12-07 14:20:19 -0500

    Updated from Brython Server: 12/7/2015 2:20:19 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,1 +1,171 @@
-print("Hello, world.")
+"""
+Space game
+Ryan Kynor
+Checklist:
+fixed star field
+~~Sun sprite~~
+at least one player
+muliple players or multplie inanimate objects to avoid
+animated rocket thrust
+collisions destroy ship
+moving ships
+rotating ships
+Source:
+lines 23 - 27 based off of orininal space game code
+"""
+
+#key list starts a line 1067 in ggame.py file
+
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+
+class Sun(Sprite):
+    """
+    The Sun on the Screen
+    """
+    
+    asset =  ImageAsset("images/sun.png")
+    height = 80
+    width = 80
+    
+    def __init__(self, position):
+        super().__init__(Sun.asset, position)
+        self.weight = 1
+        self.fxcenter = 0.5
+        self.fycenter = 0.5
+        self.circularCollisionModel()
+        
+        
+     
+class Starfield(Sprite):
+    """
+    The Starfield
+    """
+    
+    asset = ImageAsset("images/starfield.jpg")
+    #height = SCREEN_HEIGHT
+    #width = SCREEN_WIDTH
+    
+    def __init__(self, position):
+        super().__init__(Starfield.asset, position)
+        self.weight = 1
+        self.fxcenter = 0.5
+        self.fycenter = 0.5
+
+
+
+class SpaceShip(Sprite):
+    """
+    Animated space ship
+    """
+    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
+        Frame(227,0,292-227,125), 4, 'vertical')
+
+#spaceship spawn movement
+    def __init__(self, position):
+        super().__init__(SpaceShip.asset, position)
+        self.vx = 0
+        self.vy = 0
+        self.vr = 1.00
+        self.thrust = 0
+        self.thrustframe = 1
+       
+        #Thrust detection
+        SpaceGame.listenKeyEvent("keydown", "space", self.thrustOn)
+        SpaceGame.listenKeyEvent("keyup", "space", self.thrustOff)
+       
+        #Movement detection
+        SpaceGame.listenKeyEvent("keydown", "w", self.moveFowardOn)
+        SpaceGame.listenKeyEvent("keyup", "w", self.moveFowardOff)
+        
+        SpaceGame.listenKeyEvent("keydown", "s", self.moveBackOn)
+        SpaceGame.listenKeyEvent("keyup", "s", self.moveBackOff)
+        
+        SpaceGame.listenKeyEvent("keydown", "a", self.moveLeftOn)
+        SpaceGame.listenKeyEvent("keyup", "a", self.moveLeftOff)
+        
+        SpaceGame.listenKeyEvent("keydown", "d", self.moveRightOn)
+        SpaceGame.listenKeyEvent("keydown", "d", self.moveRightOff)
+        #Rotation animation
+       
+        #SpaceGame.listenKeyEvent("keydown", "w",
+        self.fxcenter = self.fycenter = 0.5
+
+#display of thrust
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+        self.rotation += self.vr
+        if self.thrust == 1:
+            self.setImage(self.thrustframe)
+            self.thrustframe += 1
+            if self.thrustframe == 4:
+                self.thrustframe = 1
+        else:
+            self.setImage(0)
+#thrust
+    def thrustOn(self, event):
+        self.thrust = 1
+    def thrustOff(self, event):
+        self.thrust = 0
+        
+#movement
+    def moveFowardOn(self,event):
+        self.foward = 1
+    def moveFowardOff(self,event):
+        self.foward = 0
+    
+    def moveRightOn(self,event):
+        self.right = 1
+    def moveRightOff(self,event):
+        self.right = 0
+        
+    def moveLeftOn(self,event):
+        self.left = 1
+    def moveLeftOff(self,event):
+        self.left = 0
+        
+    def moveBackOn(self,event):
+        self.back = 1
+    def moveBackOff(self,event):
+        self.back = 0
+        
+#rotation
+        
+        
+    
+
+
+class SpaceGame(App):
+    """
+    Tutorial4 space game example.
+    """
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        black = Color(0, 1)
+        noline = LineStyle(0, black)
+        bg_asset = RectangleAsset(width, height, noline, black)
+        bg = Sprite(bg_asset, (0,0))
+        Starfield((SCREEN_WIDTH/2,SCREEN_HEIGHT/2))
+        Sun((SCREEN_WIDTH/2,SCREEN_HEIGHT/2))
+        SpaceShip((100,100))
+        #SpaceShip((150,150))
+        #SpaceShip((200,50))
+
+    def step(self):
+        for ship in self.getSpritesbyClass(SpaceShip):
+            ship.step()
+
+
+#Sun((SCREEN_WIDTH/2,SCREEN_HEIGHT/2))
+
+myapp = SpaceGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+myapp.run()
+
+
+
+#self rotation will give you the current angle of rotation in radians
+Status API Training Shop Blog About Pricing
