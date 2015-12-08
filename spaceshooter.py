ee50d9d72e8dbdbfe727b8154e20b29109b15699
CriticalD20 <jamesnapier@hanovernorwichschools.org>
2015-12-08 14:33:10 -0500

    Updated from Brython Server: 12/8/2015 2:33:10 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -20,7 +20,7 @@ class background(Sprite):
 
 class ExplosionSmall(Sprite):
     
-    asset = ImageAsset("images/explosion1.png", Frame(0,0,128,128), 10)
+    asset = ImageAsset("images/explosion1.png", Frame(0,0,128,128))
     
     def __init__(self, position):
         super().__init__(ExplosionSmall.asset, position)
