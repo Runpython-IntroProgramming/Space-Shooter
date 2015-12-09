4857388b024eb5350f075168baaf193fe57a7647
SSupattapone <samuelsupattapone@hanovernorwichschools.org>
2015-12-09 13:32:10 -0500

    Updated from Brython Server: 12/9/2015 1:32:22 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -23,7 +23,7 @@ class SpaceShip(Sprite):
     def step(self):
         #self.x += self.vx
         #self.y += self.vy
-        self.rotation += self.turn/100
+        self.rotation += self.turn/75
         if self.thrust == 1:
             self.setImage(self.thrustframe)
             self.thrustframe += 1
