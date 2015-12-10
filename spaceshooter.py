1c030a2b2ce6a2bd9d1c0d89f839a721c333759e
SSupattapone <samuelsupattapone@hanovernorwichschools.org>
2015-12-10 14:07:57 -0500

    Updated from Brython Server: 12/10/2015 2:08:11 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -90,7 +90,7 @@ class SpaceShip(Sprite):
 class SpaceGame(App):
     def __init__(self, width, height):
         super().__init__(width, height)
-        self.ship = SpaceShip((711.5,394))
+        self.ship = SpaceShip((900,394))
         
     def step(self):
         for ship in self.getSpritesbyClass(SpaceShip):
