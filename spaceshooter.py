3f3c34cbee8da8bc2c2ad79813397637b90f5bc7
ryankynor <ryankynor@hanovernorwichschools.org>
2016-01-04 14:50:12 -0500

    Updated from Brython Server: 1/4/2016 2:50:14 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -107,6 +107,7 @@ class SpaceShip(Sprite):
             asset = ImageAsset("images/explosion2.png", Frame(0,0,128,128), 10, "horrizontal")
         if (self.y > 200) and (self.y < 250) and (self.x > 200) and (self.x < 250):
             self.explode3 = 1
+            print(self.explode3)
         else:
             self.explode3 = 0
         self.x += self.vx
