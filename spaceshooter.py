f4eab33e3139beed2bab61b63e6ddff8b0f1f1c3
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-03-03 14:51:59 -0500

    Updated from Brython Server: 3/3/2016 2:51:59 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -10,11 +10,15 @@ https://github.com/HHS-IntroProgramming/Spacewar
 
 from ggame import App, Sprite, ImageAsset, Frame
 
-starback_asset = ImageAsset("images/starfield.jpg")
-ship1_asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", Frame(0,0,85,125))#227
-
-starback = Sprite(starback_asset, (0,0))
-ship1 = Sprite(ship1_asset, (0,0))
+class starback(Sprite):
+    starback_asset = ImageAsset("images/starfield.jpg")
+    starback = Sprite(starback_asset, (0,0))
+    
+class ship1(Sprite):
+    ship1_asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", Frame(0,0,85,125))
+    def __init__(self, position):
+        super().__init__(ship1.ship1_asset, position)
+        ship1 = Sprite(ship1_asset, (0,0))
 
 myapp = App()
 myapp.run()
