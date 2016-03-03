800fbac287a28ca1bf3c77d4270620d1f64b3ac8
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-03-03 14:12:11 -0500

    Updated from Brython Server: 3/3/2016 2:12:11 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,9 +1,16 @@
 """
 spaceshooter.py
-Author: <your name here>
+Author: David Wilson
 Credit: <list sources used, if any>
 
 Assignment:
 Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
+
+from ggame import App, Sprite, ImageAsset
+
+
+
+myapp = App()
+myapp.run()
