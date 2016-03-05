f4f04764875aaf2ae34c3f84dee4d1940bd07cec
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-03-05 13:28:00 -0500

    Updated from Brython Server: 3/5/2016 1:27:59 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -13,12 +13,9 @@ from ggame import App, Sprite, ImageAsset, Frame
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 
-BACK_WIDTH = SCREEN_WIDTH/2
-BACK_HEIGHT = SCREEN_HEIGHT/2
-
 class StarBack(Sprite):
     
-    asset = ImageAsset("images/starfield.jpg", Frame(0,0,BACK_WIDTH,BACK_HEIGHT))
+    asset = ImageAsset("images/starfield.jpg", Frame(0,0,SCREEN_WIDTH/2,SCREEN_HEIGHT/2))
     
     def __init__(self, position):
         super().__init__(StarBack.asset, position)
