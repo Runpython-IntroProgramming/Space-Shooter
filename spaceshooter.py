0f772957ddd556b60f57e79efb4eea4a27e1a9c5
averywallis <averybwallis@gmail.com>
2016-03-07 14:24:55 -0500

    Updated from Brython Server: 3/7/2016 2:24:54 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -14,6 +14,15 @@ SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
 
+class SpaceShip(Sprite):
+    """
+    Animated space ship
+    """
+    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
+        Frame(227,0,292-227,125), 4, 'vertical')
+
+    def __init__(self, position):
+        super().__init__(SpaceShip.asset, position)
 
 class SpaceGame(App):
     """
