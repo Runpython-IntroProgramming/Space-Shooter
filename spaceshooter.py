f803e2163cf55baeea8ba821fefa2967d774421d
danielwilson2017 <danielwilson2017@hanovernorwichschools.org>
2016-03-09 13:56:39 -0500

    Updated from Brython Server: 3/9/2016 1:56:41 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,9 +1,20 @@
 """
 spaceshooter.py
-Author: <your name here>
+Author: Daniel Wilson
 Credit: <list sources used, if any>
 
 Assignment:
 Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
+from ggame import App, Sprite, ImageAsset, Frame
+from ggame import SoundAsset, Sound, TextAsset, Color
+import math
+from time import time
+
+    asset = ImageAsset("images/starfield.jpg")
+    width = 512
+    height = 512
+
+    def __init__(self, position):
+        super().__init__(Stars.asset, position)
