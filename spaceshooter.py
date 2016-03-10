c873a0e5853afe3d226592d2449fc663832ad7dc
averywallis <averybwallis@gmail.com>
2016-03-10 16:12:02 -0500

    Updated from Brython Server: 3/10/2016 4:12:04 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -46,8 +46,8 @@ class SpaceShip(Sprite):
         if self.thrust == 1:
             self.setImage(self.thrustframe)
             self.thrustframe += 1
-            self.vx=self.vx-self.rotation
-            sekf.vy=self.vy-self.rotation
+            self.vx=self.vx+self.vr
+            sekf.vy=self.vy+self.vr
             if self.thrustframe == 4:
                 self.thrustframe = 1
             else:
