f42d5501a1fe407eb83fb2aca72c09d98cbe074d
averywallis <averybwallis@gmail.com>
2016-03-10 14:30:38 -0500

    Updated from Brython Server: 3/10/2016 2:30:38 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -24,8 +24,8 @@ class SpaceShip(Sprite):
     def __init__(self, position):
         super().__init__(SpaceShip.asset, position)
         self.visible=True
-        self.vx = 1
-        self.vy = 1
+        self.vx = 0
+        self.vy = 0
         self.vr = 0.01
         self.thrust = 0
         self.thrustframe = 1
