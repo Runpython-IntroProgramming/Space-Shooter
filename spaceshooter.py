1638de3a359cee02f823a80f61a11e082e461272
averywallis <averybwallis@gmail.com>
2016-03-11 14:45:43 -0500

    Updated from Brython Server: 3/11/2016 2:45:42 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -27,6 +27,7 @@ class SpaceShip(Sprite):
         self.vx = 0
         self.vy = 0
         self.vr = 0
+        self.rotation=0
         self.thrust = 0
         self.thrustframe = 1
         SpaceGame.listenKeyEvent("keydown", "w", self.thrustOn)
