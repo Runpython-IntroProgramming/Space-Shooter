5a92f582cdf19928bb3678f60d10a90c0be348af
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-03-11 14:55:01 -0500

    Updated from Brython Server: 3/11/2016 2:55:01 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -178,18 +178,18 @@ class Enemy(SpaceShip):
     
     def __init__(self, position):
         super().__init__(Enemy.asset, position)
-        self.dist = 0
         self.changeDirec
         
     def changeDirec(self):
         self.rotation = randint(0,1000)/500*pi
         self.velx = 5/sin(self.rotation)
         self.vely = 5/cos(self.rotation)
+        self.dist = 0
         
     def step(self):
-        if self.dist == sqrt(SCREEN_WIDTH**2+SCREEN_HEIGHT**2) or self.x > SCREEN_WIDTH or self.x < 0 or self.y > SCREEN_HEIGHT or self.y < 0:
+        if self.dist == SCREEN_DIAG/25 or self.x > SCREEN_WIDTH or self.x < 0 or self.y > SCREEN_HEIGHT or self.y < 0:
             self.changeDirec
-        elif self.dist > 50 and randint(0,20) == 0:
+        elif self.dist > SCREEN_DIAG/5 or randint(0,20) == 0:
             self.changeDirec
         self.x += self.velx
         self.y += self.vely
