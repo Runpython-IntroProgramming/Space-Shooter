42eea0c6c4922a9b8898806cf20b0807875d1379
danielwilson2017 <danielwilson2017@hanovernorwichschools.org>
2016-03-11 14:25:19 -0500

    Updated from Brython Server: 3/11/2016 2:25:20 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -12,11 +12,23 @@ from ggame import SoundAsset, Sound, TextAsset, Color
 import math
 from time import time
 
-asset = ImageAsset("images/starfield.jpg")
-width = 512
-height = 512
+class Stars(Sprite):
+    asset = ImageAsset("images/starfield.jpg")
+    width = 512
+    height = 512
+    def __init__(self, position):
+        super().__init__(Stars.asset, position)
 
-def __init__(self, position):
-    super().__init__(Stars.asset, position)
+class Sun(Sprite):
+    asset = ImageAsset("images/sun.png")
+    width = 90
+    height = 90
+    def __init__(self, position):
+        super().__init__(Sun.asset, position)
 
-print(asset)
+class Spaceship(Sprite):
+    SpaceShip((100,100))
+    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png":
+        Frame(227,0,292-227,125), 4, 'vertical')
+    def __init__(self, position):
+        super().__init__(SpaceShip.asset, position)
