0332149eb66457fa15e729070c28b0ec12034fef
danielwilson2017 <danielwilson2017@hanovernorwichschools.org>
2016-03-11 14:32:06 -0500

    Updated from Brython Server: 3/11/2016 2:32:08 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -7,6 +7,8 @@ Assignment:
 Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
