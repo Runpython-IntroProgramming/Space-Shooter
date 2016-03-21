fd74b6fef1ec1a46db97bf755828b732ef7c93a5
phstearns <phstearns13@gmail.com>
2016-03-21 14:13:12 -0400

    Updated from Brython Server: 3/21/2016 2:13:12 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -57,9 +57,11 @@ class SpaceGame(App):
     """
     def __init__(self, width, height):
         super().__init__(width, height)
-        asset = ImageAsset("images/starfield.jpg",
+        black = Color(0, 1)
+        noline = LineStyle(0, black)
+        asset = ImageAsset("images/starfield.jpg")
         bg_asset = RectangleAsset(width, height, noline, black)
-        bg = Sprite(bg_asset, (0,0))
+        bg = Sprite(asset, (0,0))
         SpaceShip((100,100))
         SpaceShip((150,150))
         SpaceShip((200,50))
