4fbc90a6608134018ee78bbef524edf54f6d41a4
averywallis <averybwallis@gmail.com>
2016-03-21 14:48:15 -0400

    Updated from Brython Server: 3/21/2016 2:48:15 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -57,7 +57,9 @@ class SpaceShip(Sprite):
             elif math.pi/2 <= self.rotation and self.rotation <= 3*math.pi/4:
                 self.vx = -.5
                 self.vy =1.5
-                
+            elif 3*math.pi/4 <= self.rotation and self.rotation <=math.pi:
+                self.vx=-1.5
+                self.vy=.5
             if self.thrustframe == 4:
                 self.thrustframe = 1
             else:
