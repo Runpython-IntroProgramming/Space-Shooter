b738c2609a0cb68acfc80e900bf2a93e98945a67
phstearns <phstearns13@gmail.com>
2016-03-21 14:23:31 -0400

    Updated from Brython Server: 3/21/2016 2:23:32 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -7,8 +7,7 @@ Assignment:
 Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
-from ggame import App, Sprite, ImageAsset, Frame
-from ggame import SoundAsset, Sound, TextAsset, Color
+from ggame import App, Sprite, ImageAsset, Frame, SoundAsset, Sound, TextAsset, Color
 import math
 from time import time
 
