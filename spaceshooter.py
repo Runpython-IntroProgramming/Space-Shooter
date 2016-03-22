f7af7a787c88092e0f88cc201dfc9eb75889c1e7
danielwilson2017 <danielwilson2017@hanovernorwichschools.org>
2016-03-22 14:52:41 -0400

    Updated from Brython Server: 3/22/2016 2:52:40 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -53,7 +53,9 @@ class SpaceShip(Sprite):
             else:
                 self.setImage(0)
         if self.vr == 1:
-            self.rotation = self.rotation + 0.0001
+            self.rotation = self.rotation + 0.001
+        if self.vr == -1:
+            self.rotation = self.rotation + 0.001
         if self.vr == 0:
             self.rotation = 0
 
