063c275deeb7bfa32cf792d3fbca9aae71ed7419
danielwilson2017 <danielwilson2017@hanovernorwichschools.org>
2016-03-22 14:49:46 -0400

    Updated from Brython Server: 3/22/2016 2:49:46 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -53,8 +53,9 @@ class SpaceShip(Sprite):
             else:
                 self.setImage(0)
         if self.vr == 1:
-            self.rotation = self.rotation + 0.00000001
-        
+            self.rotation = self.rotation + 0.0001
+        if self.vr == 0
+            self.rotation = 0
 
     def thrustOn(self, event):
         self.thrust = 1
