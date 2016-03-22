2c134f570f2faf8bce44bdc291ea88f045d572d4
averywallis <averybwallis@gmail.com>
2016-03-22 14:18:32 -0400

    Updated from Brython Server: 3/22/2016 2:18:32 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -48,8 +48,8 @@ class SpaceShip(Sprite):
         if self.thrust == 1:
             self.setImage(self.thrustframe)
             self.thrustframe += 1
-            self.vx = self.vx + math.sin((self.rotation))
-            self.vy = self.vy + math.cos((self.rotation))
+            self.vx = self.vx + math.cos((self.rotation))
+            self.vy = self.vy + math.sin((self.rotation))
             
             """
             if 0 <= self.rotation and self.rotation <= math.pi/4:
