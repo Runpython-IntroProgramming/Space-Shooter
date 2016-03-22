de4075a1c739556df5194108c7560edeb682f1c6
danielwilson2017 <danielwilson2017@hanovernorwichschools.org>
2016-03-22 14:07:52 -0400

    Updated from Brython Server: 3/22/2016 2:07:51 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -83,7 +83,7 @@ class SpaceGame(App):
         bg_asset = RectangleAsset(width, height, noline, black)
         bg = Sprite(bg_asset, (0,0))
         SpaceShip((100,100))
-        SpaceShip((150,150))
+        SpaceShip((300,150))
         
 
     def step(self):
