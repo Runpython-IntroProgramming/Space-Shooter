3885058abf207e5700e64056d222589fc8f83b0f
averywallis <averybwallis@gmail.com>
2016-03-25 14:31:37 -0400

    Updated from Brython Server: 3/25/2016 2:31:37 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -56,9 +56,9 @@ class SpaceShip(Sprite):
             else:
                 self.setImage(0)
         if self.vr == .1:
-            self.rotation = self.rotation+.0001
+            self.rotation += .0001
         if self.vr == -.1:
-            self.rotation =self.rotation-.0001
+            self.rotation -= .0001
         if self.vr==0:
             self.rotation=self.rotation
             
