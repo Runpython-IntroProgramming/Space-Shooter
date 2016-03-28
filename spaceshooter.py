62523ef9ba82273a81d35b5c7d6bad412b8fff01
averywallis <averybwallis@gmail.com>
2016-03-28 14:37:00 -0400

    Updated from Brython Server: 3/28/2016 2:36:59 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -146,7 +146,7 @@ class SpaceGame(App):
         black = Color(0, 1)
         noline = LineStyle(0, black)
         bg_asset = ImageAsset("images/starfield.jpg")
-        txt_asset = TextAsset(right, "Press R to restart")
+        txt_asset = TextAsset("Press R to restart",right,black,italic 20pt Helvetica)
         txt = Sprite(txt_asset, (0,0))
         star0 = Sprite(bg_asset, (0,0))
         star1= Sprite(bg_asset, (512,0))
