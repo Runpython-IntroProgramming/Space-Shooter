bcc55a39c5cc5cfa01b7267469599e7802cb84b6
averywallis <averybwallis@gmail.com>
2016-03-29 14:12:33 -0400

    Updated from Brython Server: 3/29/2016 2:12:33 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -125,7 +125,7 @@ class SpaceShip2(Sprite):
         SpaceGame.listenKeyEvent("keydown", "right arrow" , self.CCthing2)
         SpaceGame.listenKeyEvent("keyup", "right arrow", self.CCOff2)
         SpaceGame.listenKeyEvent("keydown", "p", self.restartOn2)
-        SpaceGame.listenKeyEvent("keyup", "", self.restartOff2)
+        SpaceGame.listenKeyEvent("keyup", "p", self.restartOff2)
         self.fxcenter = self.fycenter = 0.5
         
     def step(self):
