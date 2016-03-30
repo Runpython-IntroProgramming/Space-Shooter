e192681efb2146df376d0f688b33cbb2556675b1
averywallis <averybwallis@gmail.com>
2016-03-30 13:37:37 -0400

    Updated from Brython Server: 3/30/2016 1:37:37 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -66,7 +66,7 @@ class SpaceShip(Sprite):
         col= self.collidingWithSprites(Sunthing)
         if col:
             self.explode()
-            time.sleep(1)
+            time.sleep(.5)
             self.reset = 1
             
         if self.reset == 1:
