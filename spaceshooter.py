bf1a45b3441a2c159baac6241f11c60b1ca2848f
phstearns <phstearns13@gmail.com>
2016-04-01 14:48:34 -0400

    Updated from Brython Server: 4/1/2016 2:48:34 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -55,9 +55,9 @@ class SpaceShip(Sprite):
             else:
                 self.setImage(0)
         if self.vr == .1:
-            self.rotation = self.rotation +100
+            self.rotation = self.rotation +.0001
         if self.vr == -0.1:
-            self.rotation = self.rotation - 100
+            self.rotation = self.rotation -.0001
         
         col=self.collidingWithSprites(Sun)
         if col:
