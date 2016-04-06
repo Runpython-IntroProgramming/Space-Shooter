0528df4e0963387b31cd558af625661353c7583b
averywallis <averybwallis@gmail.com>
2016-04-06 13:25:49 -0400

    Updated from Brython Server: 4/6/2016 1:25:49 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -191,7 +191,7 @@ class SpaceShip2(Sprite):
         if self.reset == 0:
             self.x = self.x
             self.y = self.y
-            self.visible = True
+            self.visible = False
             
         if self.reset == 2:
             self.visible = True
