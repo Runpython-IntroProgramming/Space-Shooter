7e5ef901884f640a29fc516579455cfebf64d33c
nilskingston <nilskingston@hanovernorwichschools.org>
2016-04-18 14:47:18 -0400

    Updated from Brython Server: 4/18/2016 2:47:19 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -9,13 +9,11 @@ https://github.com/HHS-IntroProgramming/Spacewar
 """
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
 
-SCREEN_WIDTH = 640
+SCREEN_WIDTH = 1250
 SCREEN_HEIGHT = 480
 
 class SpaceGame(App):
-    """
-    Tutorial4 space game example.
-    """
+ 
     def __init__(self, width, height):
         super().__init__(width, height)
         black = Color(0, 1)
