ef286abb5c37dc31bf8337f10f0e3526d9fa813a
nilskingston <nilskingston@hanovernorwichschools.org>
2016-04-18 14:49:20 -0400

    Updated from Brython Server: 4/18/2016 2:49:21 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -12,6 +12,14 @@ from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Fra
 SCREEN_WIDTH = 1250
 SCREEN_HEIGHT = 600
 
+class SpaceShip(Sprite):
+  
+    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
+        Frame(227,0,292-227,125), 4, 'vertical')
+
+    def __init__(self, position):
+        super().__init__(SpaceShip.asset, position)
+
 class SpaceGame(App):
  
     def __init__(self, width, height):
@@ -20,6 +28,7 @@ class SpaceGame(App):
         noline = LineStyle(0, black)
         bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
         bg = Sprite(bg_asset, (0,0))
+        SpaceShip((100,100))
 
 myapp = SpaceGame(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
