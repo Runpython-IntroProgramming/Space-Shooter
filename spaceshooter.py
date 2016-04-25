c1006c008cffd100fe018c7d719caaa3e0c25a82
willcampbel <williamcampbell@hanovernorwichschools.org>
2016-04-25 14:31:48 -0400

    Updated from Brython Server: 4/25/2016 2:31:48 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,5 +1,5 @@
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
-
+from math import sin, cos
 
 class Stars(Sprite):
 
@@ -23,13 +23,21 @@ class Ship(Sprite):
         Frame(227,0,292-227,125), 4, 'vertical')
     def __init__(self, position):
         super().__init__(Ship.asset, position)
+        self.fxcenter = self.fycenter = 0.5
         SpaceGame.listenKeyEvent('keydown', 'right arrow', self.rotateRight)
         SpaceGame.listenKeyEvent('keydown', 'left arrow', self.rotateLeft)
-        
+        SpaceGame.listenKeyEvent('keydown', 'up arrow', self.moveUp)
+        SpaceGame.listenKeyEvent('keydown', 'down arrow', self.moveDown)
     def rotateRight(self, event):
-        self.rotation -= 1
+        self.rotation -= 0.1
     def rotateLeft(self, event):
-        self.rotation += 1
+        self.rotation += 0.1
+    def moveUp(self, event):
+        self.x += -5*sin(self.rotation)
+        self.y += -5*cos(self.rotation)
+    def moveDown(self, event):
+        self.rotation += 0.1
+        
     
 class SpaceGame(App):
     
