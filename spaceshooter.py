077d4e48979d2b49683806021b020319acbfe22c
Hagin Onyango <hagincubes@gmail.com>
2016-04-26 17:52:31 -0400

    Updated from Brython Server: April 26, 2016 5:52:35 PM EDT

--- spaceshooter.py
+++ spaceshooter.py
@@ -11,3 +11,7 @@ from ggame import App, Sprite, ImageAsset, Frame, Color, TextAsset, SoundAsset, 
 from math import sqrt, sin, cos, radians, degrees, pi, atan
 from random import randint
 from time import sleep
+
+SCREEN_WIDTH = 1000 #1200
+SCREEN_HEIGHT = 600 #700
+SCREEN_DIAG = sqrt(SCREEN_WIDTH**2+SCREEN_HEIGHT**2)
