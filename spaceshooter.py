dfcde05d6c4fc9e74a693f2f9afe60c4cc04a695
Hagin Onyango <hagincubes@gmail.com>
2016-04-29 15:19:59 -0400

    Updated from Brython Server: April 29, 2016 3:20:01 PM EDT

--- spaceshooter.py
+++ spaceshooter.py
@@ -16,12 +16,18 @@ SCREEN_WIDTH = 1000 #1200
 SCREEN_HEIGHT = 600 #700
 SCREEN_DIAG = sqrt(SCREEN_WIDTH**2+SCREEN_HEIGHT**2)
 
-class SpaceShip(Sprite):
-    """
-    Animated space ship
-    """
-    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
-        Frame(227,0,292-227,125), 4, 'vertical')
 
-    def __init__(self, position):
-        super().__init__(SpaceShip.asset, position)
+if SCREEN_WIDTH >= SCREEN_HEIGHT:
+    LARGER_SIDE = SCREEN_WIDTH
+    SMALLER_SIDE = SCREEN_HEIGHT
+else:
+    LARGER_SIDE = SCREEN_HEIGHT
+    SMALLER_SIDE = SCREEN_WIDTH
+    
+white = Color (0x#ffffff, 1.0) 
+
+velocityOfX = velocity, rotation: -1*speed*sin(rotation)
+velocityOfY = velocity, rotation: -1*speed*cos(rotation)
+
+def RektClass(sclass):
+    whi
