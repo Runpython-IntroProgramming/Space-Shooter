7daeba20c651cb992bc5b1497064f212a1c6b55a
Hagin Onyango <hagincubes@gmail.com>
2016-05-15 18:13:21 -0400

    Updated from Brython Server: May 15, 2016 6:13:24 PM EDT

--- spaceshooter.py
+++ spaceshooter.py
@@ -8,270 +8,136 @@ Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
 from ggame import App, RectangleAsset, ImageAsset, SoundAsset, Sprite, Sound, LineStyle, Color, Frame
-
 import math
-
 import time
-
 SCREEN_WIDTH = 1536
-
 SCREEN_HEIGHT = 1024
-
 class Sun(Sprite):
 
-    
-
     asset = ImageAsset("images/sun.png")
-
     width = 80
-
     height = 76
 
-    
-
     def __init__(self, position):
-
         super().__init__(Sun.asset, position)
-
         self.fxcenter = 0.5
-
         self.fycenter = 0.5
-
         self.circularCollisionModel()
 
 
 
-
-
-
-
 class Ship1(Sprite):
-
     
-
     asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png",
-
         Frame(227,0,292-227,125), 4, 'vertical')
 
 
-
-
     def __init__(self, position):
-
         super().__init__(Ship1.asset, position)
-
         self.vr = 0.00
-
         self.thrust = 0
-
         self.thrustframe = 1
-
         self.VX = 0
-
         self.VY = 0   
-
         self.vx = 0
-
         self.vy = 0
-
         self.turn = 0
-
         SpaceGame.listenKeyEvent("keydown", "up arrow", self.thrustOn)
-
         SpaceGame.listenKeyEvent("keyup", "up arrow", self.thrustOff)
-
         SpaceGame.listenKeyEvent("keydown", "left arrow", self.rotateLeft)
-
         SpaceGame.listenKeyEvent("keyup", "left arrow", self.lrOff)
-
         SpaceGame.listenKeyEvent("keydown", "right arrow", self.rotateRight)
-
         SpaceGame.listenKeyEvent("keyup", "right arrow", self.rrOff)
-
         SpaceGame.listenKeyEvent("keypress", "enter", self.fire)
-
         self.fxcenter = self.fycenter = 0.5
-
         self.bullet = None
-
     
-
     def step(self):
-
         self.rotation += 1.5*self.vr
-
         self.move()
-
         if self.thrust == 1:
-
             self.VX += self.vx
-
             self.VY += self.vy
-
         if 0 <= self.x <= SCREEN_WIDTH:
-
             self.x -= 0.1*self.VX
-
         elif self.x < 0:
-
             self.x += SCREEN_WIDTH
-
             self.x -= 0.1*self.VX
-
         else:    
-
             self.x -= (0.1*self.VX + SCREEN_WIDTH)
-
         if 0 <= self.y <= SCREEN_HEIGHT:    
-
             self.y -= 0.1*self.VY
-
         elif self.y < 0:
-
             self.y += SCREEN_HEIGHT
-
             self.y -= 0.1*self.VY
-
         else:
-
             self.y -= (0.1*self.VY + SCREEN_HEIGHT)
-
     
-
         if self.thrust == 1:
-
             self.setImage(self.thrustframe)
-
             self.thrustframe += 1
-
             if self.thrustframe == 4:
-
                 self.thrustframe = 1
-
         else:
-
             self.setImage(0)
-
             
-
         collides = self.collidingWithSprites(Ship2)
-
         collides.extend(self.collidingWithSprites(Bullet))
-
         if len(collides):
-
             if collides[0].visible:
-
                 collides[0].explode()
-
                 self.explode()
-
     
-
     def move(self):
-
         self.X = math.sin(self.rotation)
-
         self.Y = math.cos(self.rotation)
-
         self.vx = self.X/math.sqrt(self.X*self.X + self.Y*self.Y)
-
         self.vy = self.Y/math.sqrt(self.X*self.X + self.Y*self.Y)
-
     
-
     def explode(self):
-
         self.visible = False
-
         ExplosionBig(self.position)
-
         self.waitspawn = 5
-
     
-
     def thrustOn(self, event):
-
         self.thrust = 1
-
         
-
     def thrustOff(self, event):
-
         self.thrust = 0
-
         
-
     def rotateLeft(self, event):
-
         self.vr = 0.05
-
         
-
-    def lrOff(self,  event):
-
+    def lrOff(self, event):
         self.vr = 0
-
         
-
     def rotateRight(self, event):
-
         self.vr = -0.05
-
-        
-
-    def rrOff(self,  event):
-
+    def rrOff(self, event):
         self.vr = 0
-
-    
-
     def fire(self, event):
-
         self.bullet= Bullet(self.position, self.vx, self.vy)
-
-
-
 
 class Ship2(Sprite):
 
-    
-
     asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
-
         Frame(0,0,86,125), 4, 'vertical')
 
 
-
-
     def __init__(self, position):
-
         super().__init__(Ship2.asset, position)
-
         self.vr = 0.00
-
         self.thrust = 0
-
         self.thrustframe = 1
-
         self.VX = 0
-
         self.VY = 0   
-
         self.vx = 0
-
         self.vy = 0
-
         self.turn = 0
-
         SpaceGame.listenKeyEvent("keydown", "w", self.thrustOn)
-
         SpaceGame.listenKeyEvent("keyup", "w", self.thrustOff)
-
         SpaceGame.listenKeyEvent("keydown", "a", self.rotateLeft)
-
         SpaceGame.listenKeyEvent("keyup", "a", self.lrOff)
-
         SpaceGame.listenKeyEvent("keydown", "d", self.rotateRight)
         SpaceGame.listenKeyEvent("keyup", "d", self.rrOff)
         SpaceGame.listenKeyEvent("keypress", "e", self.fire)
@@ -450,7 +316,7 @@ class ExplosionBig(Sprite):
         if self.image == 50:
             self.destroy()
 class SpaceGame(App):
-    def __init__(self, width, height)
+    def __init__(self, width, height):
         super().__init__(width, height)
         black = Color(0, 1)
         noline = LineStyle(0, black)
