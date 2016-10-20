428ecbaf1fbefd3038f5e68779d3bfd1379cac74
TheBigBlueBlob <liamszczepiorkowski@hanovernorwichschools.org>
2016-10-20 10:48:21 -0400

    Updated from Brython Server: 10/20/2016 10:48:20 AM

--- spaceshooter.py
+++ spaceshooter.py
@@ -44,9 +44,9 @@ class SpaceShip(Sprite):
         SpaceGame.listenKeyEvent("keydown", "right arrow", self.thrustRight)
     def step(self):
         if self.sideThrust == 1:
-            self.vAddedx += 1
+            self.vAddedx += 0.5
         if self.sideThrust == -1:
-            self.vAddedx -= 1
+            self.vAddedx -= 0.5
         if self.sideThrust == 0:
             self.vAddedx += 0
         self.x += self.vAddedx
