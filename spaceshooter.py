871c6e849516c38b3043856715e7a3b2f55483b8
VinzentM <vinzentmoesch@hanovernorwichschools.org>
2016-10-28 13:59:49 -0400

    Updated from Brython Server: 10/28/2016 1:59:49 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -7,19 +7,82 @@ Assignment:
 Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
-#import the stuff
+"""
+tutorial4.py
+by E. Dennison
+"""
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
 from ggame import App, Sprite, ImageAsset, Frame
 from ggame import SoundAsset, Sound, TextAsset, Color
 import math
 from time import time
 
-
-#background
-class background(Sprite):
+#Hintergrund
+class Stars(Sprite):
 
     asset = ImageAsset("images/Stars, background.jpg")
-    width = 1024
-    height = 1024
+    width = 1920
+    height = 1080
 
     def __init__(self, position):
         super().__init__(Stars.asset, position)
+
+
+class SpaceShip(Sprite):
+    """
+    Animated space ship
+    """
+    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
+        Frame(227,0,292-227,125), 4, 'vertical')
+
+    def __init__(self, position):
+        super().__init__(SpaceShip.asset, position)
+        self.vx = 0
+        self.vy = 0
+        self.vr = 0.02
+        self.thrust = 0
+        self.thrustframe = 1
+        SpaceGame.listenKeyEvent("keydown", "space", self.thrustOn)
+        SpaceGame.listenKeyEvent("keyup", "space", self.thrustOff)
+        self.fxcenter = self.fycenter = 0.5
+
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+        self.rotation += self.vr
+        if self.thrust == 1:
+            self.setImage(self.thrustframe)
+            self.thrustframe += 1
+            if self.thrustframe == 4:
+                self.thrustframe = 1
+        else:
+            self.setImage(0)
+
+    def thrustOn(self, event):
+        self.thrust = 1
+
+    def thrustOff(self, event):
+        self.thrust = 0
+
+
+
+class SpaceGame(App):
+    """
+    Tutorial4 space game example.
+    """
+    def __init__(self, width, height):
+        super().__init__(width, height)
+#        black = Color(0, 1)
+#        noline = LineStyle(0, black)
+#        bg_asset = RectangleAsset(width, height, noline, black)
+#        bg = Sprite(bg_asset, (0,0))
+        SpaceShip((100,100))
+
+
+    def step(self):
+        for ship in self.getSpritesbyClass(SpaceShip):
+            ship.step()
+
+
+myapp = SpaceGame(0, 0)
+myapp.run()
