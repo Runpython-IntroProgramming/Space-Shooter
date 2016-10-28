a9579c58745b6ca568d175a4563757d2d2a20879
WilsonRimberg <wilsonrimberg@hanovernorwichschools.org>
2016-10-28 14:00:24 -0400

    Update spaceshooter.py

--- spaceshooter.py
+++ spaceshooter.py
@@ -9,19 +9,22 @@ https://github.com/HHS-IntroProgramming/Spacewar
 """
 from ggame import App, RectangleAsset, ImageAsset, SoundAsset
 from ggame import LineStyle, Color, Sprite, Sound, Frame
-SCREEN_WIDTH = 640
-SCREEN_HEIGHT = 480
+SCREEN_WIDTH = 1420
+SCREEN_HEIGHT = 810
 
 green = Color(0x00ff00, 1)
 black = Color(0, 1)
 noline = LineStyle(0, black)
-bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
-bg = Sprite(bg_asset, (0,0))
-ball_asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
-        Frame(227,0,292-227,125), 4, 'vertical')
-spaceship = Sprite(ball_asset, (0, 0))
+space_asset= ImageAsset("images/starfield.jpg",)
+space_asset2=ImageAsset("images/starfield.jpg",Frame(0,0,250,250))
+space=Sprite(space_asset, (0,0))
+space2=Sprite(space_asset,)
+spaceship_asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
+        Frame(0,0,292-227,125), 4, 'vertical')
+spaceship = Sprite(spaceship_asset, (0, 0))
 # Movement
 spaceship.dir = 1
+spaceship.bob=1
 spaceship.go = False
 spaceship.ygo= False
 def reverse(b):
@@ -31,9 +34,9 @@ def left(b):
 def right(b):
     spaceship.dir=1
 def up(b):
-    spaceship.dir=-1
+    spaceship.bob=-1
 def down(b):
-    spaceship.dir=1
+    spaceship.bob=1
 # Step
 def step():
     print("step")
@@ -47,9 +50,9 @@ def step():
 def ystep():
     print("ystep")
     if spaceship.ygo:
-        spaceship.y += spaceship.dir
+        spaceship.y += spaceship.bob
         if spaceship.y +spaceship.height > SCREEN_HEIGHT or spaceship.y < 0:
-            spaceship.y -= spaceship.dir
+            spaceship.y -= spaceship.bob
             reverse(spaceship)
 # Handle the space key
 def spaceKey(event):
@@ -93,12 +96,8 @@ myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.listenKeyEvent('keydown', 'space', spaceKey)
 myapp.listenMouseEvent('click', mouseClick)
 myapp.listenKeyEvent('keydown', 'a', leftKey)
-myapp.listenKeyEvent('keyup', 'a', LeftUp)
 myapp.listenKeyEvent('keydown', 'd', rightKey)
-myapp.listenKeyEvent('keyup', 'd', RightUp)
 myapp.listenKeyEvent('keydown', 'w', upKey)
-myapp.listenKeyEvent('keyup', 'w', UpUp)
 myapp.listenKeyEvent('keydown', 's', downKey)
-myapp.listenKeyEvent('keyup', 's', DownUp)
 #myapp.run(ystep)
 myapp.run(step)
