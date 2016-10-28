43eeba055570341b5e6ba783029b2207f22e3dc3
BautiG <juanbautistagallino@hanovernorwichschools.org>
2016-10-28 14:00:21 -0400

    Update spaceshooter.py

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,9 +1,23 @@
 """
 spaceshooter.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Bauti Gallino
+Credit: none
 
 Assignment:
 Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
+from ggame import App, Sprite, ImageAsset, Frame
+from ggame import SoundAsset, Sound, TextAsset, Color
+import math
+from time import time
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+class Star(Sprite):
+    asset = ImageAsset("images/starfield.jpg")
+    
+    def _init_ (self, width, height, positioning)
+    self.w = width
+    self.h = height
