67bfc81ccc9d054b57d899c57241709d226888b7
VinzentM <vinzentmoesch@hanovernorwichschools.org>
2016-10-31 13:32:17 -0400

    Updated from Brython Server: 10/31/2016 1:32:17 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -20,7 +20,7 @@ from time import time
 #Hintergrund
 class Stars(Sprite):
 
-    asset = ImageAsset("images/starfield.jpg")
+    asset = ImageAsset("images/starswithoutspace.jpg")
     width = 1920
     height = 1080
 
