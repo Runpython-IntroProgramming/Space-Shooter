46096e2f8f71aa76930c7cdb82e833a0b6187204
WilsonRimberg <wilsonrimberg@hanovernorwichschools.org>
2016-11-08 13:47:09 -0500

    Updated from Brython Server: 11/8/2016 1:47:09 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -25,8 +25,10 @@ space5=Sprite(space_asset, (512,512))
 space6=Sprite(space_asset, (1024, 512))
 spaceship_asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
         Frame(227,0,292-227,125), 4, 'vertical')
+asteroid_asset=ImageAsset("images/asteroid_for_program.png",)
+asteroid=Sprite(asteroid_asset, (300,300))
 
-spaceship = Sprite(spaceship_asset, (0, 0))
+spaceship = Sprite(spaceship_asset, (740, 405))
 spaceship.fxcenter = spaceship.fycenter = 0.5
 # Movement
 spaceship.dir = 1
@@ -37,6 +39,7 @@ spaceship.thrust = 0
 spaceship.thrustframe = 1
 def reverse(b):
     b.dir *= -1
+    b.bob *= -1
 def left(b):
     spaceship.dir=-3
 def right(b):
@@ -52,8 +55,13 @@ def down(b):
 def step():
     if spaceship.go:
         spaceship.x += spaceship.dir
-        if spaceship.x + spaceship.width > SCREEN_WIDTH or spaceship.x < 0:
+        if spaceship.x + spaceship.width > SCREEN_WIDTH:
             spaceship.x -= spaceship.dir
+            spaceship.rotation=(3.141592653589793238462643383/2)
+            reverse(spaceship)
+        if spaceship.x < 60:
+            spaceship.x -= spaceship.dir
+            spaceship.rotation=((3*3.141592653589793238462643383)/2)
             reverse(spaceship)
         if spaceship.thrust == 1:
             spaceship.setImage(spaceship.thrustframe)
@@ -67,8 +75,13 @@ def step():
 def ystep():
     if spaceship.ygo:
         spaceship.y += spaceship.bob
-        if spaceship.y +spaceship.height > SCREEN_HEIGHT or spaceship.y < 0:
+        if spaceship.y +spaceship.height > SCREEN_HEIGHT+60:
             spaceship.y -= spaceship.bob
+            spaceship.rotation=0
+            reverse(spaceship)
+        if spaceship.y < 60:
+            spaceship.y -= spaceship.bob
+            spaceship.rotation=3.141592653589793238462643383
             reverse(spaceship)
         if spaceship.thrust == 1:
             spaceship.setImage(spaceship.thrustframe)
