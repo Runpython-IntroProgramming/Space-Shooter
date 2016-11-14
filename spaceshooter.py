eceecd1d57f020b3ae4e31c2625450b7dd7a3b16
VinzentM <vinzentmoesch@hanovernorwichschools.org>
2016-11-14 14:00:20 -0500

    Updated from Brython Server: 11/14/2016 2:00:25 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -36,18 +36,24 @@ class astroid(Sprite):
     
     def __init__(self, position):
         super().__init__(astroid.asset, position)    
-        self.avx = 0
+        self.avx = 1
         self.avy = 0
-        self.avr = 1
+        self.avr = 0.05
         self.random1 = round((random.random())*100)
-        self.ranom2 = round((random.random())*10)
+        self.random2 = round((random.random())*10)
         number = 0
-    def step(self):
-        if number >= random1:
-            self.avx = random1
-            number = 0
-        number += 12
+        self.fxcenter = self.fycenter = 0.5
         
+    def step(self):
+       # if number <= random1:
+       #     self.avx = random2
+       #     number = 0
+      #  number += 11
+
+        
+        self.rotation += self.avr
+        self.x += self.avx
+        self.y += self.avy
 class SpaceShip(Sprite):
     """
     Animated space ship
@@ -153,7 +159,7 @@ class SpaceGame(App):
         super().__init__(width, height)
         Stars((0,0))
         SpaceShip((500,500))
-        astroid((0,0))
+        astroid((200,150))
 
 
     def step(self):
@@ -161,7 +167,6 @@ class SpaceGame(App):
             ship.step()
         for Bstroid in self.getSpritesbyClass(astroid):
             Bstroid.step()
-
-
+            
 myapp = SpaceGame(1900, 950)
 myapp.run()
