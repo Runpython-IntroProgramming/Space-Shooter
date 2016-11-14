55c20a8a88691fdec42434ac62785e1a032f7ece
BautiG <juanbautistagallino@hanovernorwichschools.org>
2016-11-14 14:03:04 -0500

    Updated from Brython Server: 11/14/2016 2:03:04 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -36,8 +36,7 @@ class Spaceship(Sprite):
         self.Velocityspaceshipleft=0
         self.Velocityspaceshipdown=0
         self.Velocityspaceshipup=0
-        self.Rotaterightspaceship=0
-        self.Rotateleftspaceship=0
+        self.Rotatespaceship=0
         SpaceGame.listenKeyEvent("keydown", "space", self.Engineon)
         SpaceGame.listenKeyEvent("keyup", "space", self.Engineoff)
         SpaceGame.listenKeyEvent("keydown", "right arrow", self.Velocityright)
@@ -53,8 +52,10 @@ class Spaceship(Sprite):
         SpaceGame.listenKeyEvent("keydown", "o", self.Rotateleft)
         SpaceGame.listenKeyEvent("keyup", "o", self.Rotateleftstop)
     def step(self):
-        if self.Rotaterightspaceship:
+        if self.Rotatespaceship==1:
             self.rotation=self.rotation+.3
+        elif self.Rotationspaceship==-1:
+            self.rotation=self.rotation-.3
         else:
             self.rotation=self.rotation
         if self.Velocityspaceshipup==1:
@@ -102,13 +103,13 @@ class Spaceship(Sprite):
     def Velocityupstop(self, event):
         self.Velocityspaceshipdown=0
     def Rotateright(self, event):
-        self.Rotaterightspaceship=1
+        self.Rotatespaceship=1
     def Rotaterightstop(self, event):
-        self.Rotaterightspaceship=0
+        self.Rotatespaceship=0
     def Rotateleft(self, event):
-        self.Rotateleftspaceship=1
-    def Rotateleft(self, event):
-        self.Rotateleftspaceship=0
+        self.Rotatespaceship=-1
+    def Rotateleftstop(self, event):
+        self.Rotatespaceship=0
 class SpaceGame(App):
     def __init__(self, width, height):
         super().__init__(width, height)
