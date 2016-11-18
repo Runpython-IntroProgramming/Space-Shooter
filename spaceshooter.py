60c15ea57f8afc4459c27e17fc90504a2cdfb859
VinzentM <vinzentmoesch@hanovernorwichschools.org>
2016-11-18 13:55:59 -0500

    Updated from Brython Server: 11/18/2016 1:55:59 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -135,9 +135,9 @@ class SpaceShip(Sprite):
             self.setImage(self.thrustframe)
             self.imagenumber += 1
             if self.imagenumber == 9:
-                self.thrustframe += 1
+                self.thrustframe += 3
                 if self.thrustframe >= 7:
-                    self.thrustframe = 4
+                    self.thrustframe = 2
                 self.imagenumber = 0
 
 
