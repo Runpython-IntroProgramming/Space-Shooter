6ec0aeb227c5a7df3faf7fc0de01112f5103adfb
marcusjdh <marcushelble@hanovernorwichschools.org>
2016-11-28 13:17:41 -0500

    Updated from Brython Server: November 28, 2016 1:17:40 PM EST

--- spaceshooter.py
+++ spaceshooter.py
@@ -24,22 +24,12 @@ space4=Sprite(space_asset, (0,512))
 space5=Sprite(space_asset, (512,512))
 space6=Sprite(space_asset, (1024, 512))
 class SpaceShip(Sprite):
-    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png",
+    """
+    Animated space ship
+    """
+    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
         Frame(227,0,292-227,125), 4, 'vertical')
-    def __init__(self, position):
-        super().__init__(SpaceShip.asset, position)
-        self.vr = 0.01
-        self.thrust = 0
-        self.thrustframe = 1
-        self.rx = 1
-        self.ry = -1
-        self.rxa = 0
-        self.rxb = 0
-        self.rya = 0
-        self.ryb = 0
-        self.c = 0
-        self.d = 0
-        self.visible = True
+
     def __init__(self, position):
         super().__init__(SpaceShip.asset, position)
         self.vx = 1
@@ -68,7 +58,13 @@ class SpaceShip(Sprite):
 
     def thrustOff(self, event):
         self.thrust = 0
+
+
+
 class SpaceGame(App):
+    """
+    Tutorial4 space game example.
+    """
     def __init__(self, width, height):
         super().__init__(width, height)
         black = Color(0, 1)
@@ -79,59 +75,12 @@ class SpaceGame(App):
         SpaceShip((150,150))
         SpaceShip((200,50))
 
-
     def step(self):
         for ship in self.getSpritesbyClass(SpaceShip):
             ship.step()
 
 
-    def step(self):
-        for ship in self.getSpritesbyClass(SpaceShip):
-            ship.step()
 
-
-        SpaceGame.listenKeyEvent("keydown", "left arrow", self.moveL)
-        SpaceGame.listenKeyEvent("keydown", "right arrow", self.moveR)
-        SpaceGame.listenKeyEvent("keydown", "down arrow", self.moveD)
-        SpaceGame.listenKeyEvent("keydown", "up arrow", self.moveU)
-       
-        SpaceGame.listenKeyEvent("keyup", "left arrow", self.nMoveL) 
-        SpaceGame.listenKeyEvent("keyup", "right arrow", self.nMoveR) 
-        SpaceGame.listenKeyEvent("keyup", "down arrow", self.nMoveD) 
-        SpaceGame.listenKeyEvent("keyup", "up arrow", self.nMoveU) 
-        self.fxcenter = self.fycenter = 0.5
-    def step(self):
-        self.rotation = 0
-        if self.thrust == 1:
-            self.setImage(self.thrustframe)
-            self.thrustframe += 1
-            if self.thrustframe == 4:
-                self.thrustframe = 1
-        else:
-            self.setImage(0)
-        if self.rxa == 2 and self.rxb == 2:
-            self.x=self.x
-            self.c=0
-        else:
-            self.c=1
-        if self.rx == 5:
-            self.x=self.x+10
-            self.c=2
-        if self.rya == 2 and self.ryb == 2:
-            self.y=self.y
-            self.d=0
-        else:
-            if self.ry == -5:
-                self.y=self.y-10
-                self.d=1
-            if self.ry == 5:
-                self.y=self.y+10
-                self.d = 10
-            if self.c==0 and self.d==0:
-                self.rotation = 0
-                self.thrust = 0
-            else:
-                self.thrust=1
 
 
     def __init__(self, width, height):
@@ -151,7 +100,7 @@ def leftKey(event):
    spaceship.go = True
    spaceship.ygo= False
    spaceship.thrust = 1
-   spaceship.rotation=(3.141592653589793238462643383/2)
+   spaceship.rotation=(pi/2)
    left(spaceship)
 
 
@@ -160,7 +109,7 @@ def rightKey(event):
    spaceship.go = True
    spaceship.ygo=False
    spaceship.thrust = 1
-   spaceship.rotation=(3.141592653589793238462643383*3)/2
+   spaceship.rotation=(pi/2)
    right(spaceship)
   
 def upKey(event):
@@ -174,11 +123,8 @@ def downKey (event):
    spaceship.ygo = True
    spaceship.go = False
    spaceship.thrust = 1
-   spaceship.rotation=3.141592653589793238462643383
+   spaceship.rotation=pi
    down(spaceship)
-
-
-
-
+   
 myapp = SpaceGame(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
