fd0a8f190c4c317b95e11312a42024b35bc9bab4
marcusjdh <marcushelble@hanovernorwichschools.org>
2016-11-29 13:52:26 -0500

    Updated from Brython Server: November 29, 2016 1:52:24 PM EST

--- spaceshooter.py
+++ spaceshooter.py
@@ -10,8 +10,8 @@ https://github.com/HHS-IntroProgramming/Spacewar
 import ggame
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
 from random import random
-SCREEN_WIDTH = 1500
-SCREEN_HEIGHT = 910
+SCREEN_WIDTH = 512
+SCREEN_HEIGHT = 512
 pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
 
 
@@ -46,11 +46,11 @@ class SpaceShip(Sprite):
         SpaceGame.listenKeyEvent("keydown", "left arrow", self.left)
         SpaceGame.listenKeyEvent("keyup", "left arrow", self.stopleft)
         SpaceGame.listenKeyEvent("keydown", "right arrow", self.right)
-        SoaceGame.listenKeyEvent("keyup", "right arrow", self.stopright)
+        SpaceGame.listenKeyEvent("keyup", "right arrow", self.stopright)
         SpaceGame.listenKeyEvent("keydown", "up arrow", self.up)
         SpaceGame.listenKeyEvent("keyup", "up arrow", self.stopup)
         SpaceGame.listenKeyEvent("keydown", "down arrow", self.down)
-        Game.listenKeyEvent("keyup", "down arrow", self.stopdown)
+        SpaceGame.listenKeyEvent("keyup", "down arrow", self.stopdown)
         
         self.fxcenter = self.fycenter = 0.5
 
@@ -66,11 +66,86 @@ class SpaceShip(Sprite):
         else:
             self.setImage(0)
 
+        if self.rxa == 2 and self.rxb == 2:
+            self.x=self.x
+            self.c = 0
+        
+        else:
+            if self.rx == -5: 
+                self.x=self.x-10
+                self.c=1
+            if self.rx == 5: 
+                self.x=self.x+10
+                self.c=2
+      
+            if self.rya == 2 and self.ryb == 2:
+                self.y=self.y
+                self.d = 0
+            
+            else: 
+                if self.ry == -5: 
+                    self.y=self.y-10
+                    self.d = 1
+                if self.ry == 5: 
+                    self.y=self.y+10
+                    self.d = 2
+                if self.c==0 and self.d==0:
+                    self.rotation = 0
+                    self.thrust = 0
+                else:
+                    self.thrust=1
+                    if self.c==1: 
+                        if self.d==1:
+                            self.rotation=(1/4)*pi
+                    else:
+                        if self.d==2: 
+                            self.rotation=(3/4)*pi
+                        else:
+                            self.rotation=pi/2
+                            if self.c==2: 
+                                if self.d==1: 
+                                    self.rotation=(7/4)*pi
+                                else:
+                                    if self.d==2: 
+                                        self.rotation=(5/4)*pi
+                                    else:
+                                        self.rotation=(3/2)*pi
+                                        if self.d==1: 
+                                                 self.rotation=0
+            
+                                        if self.d==2: 
+                                            self.rotation=pi
+                                            collision = self.collidingWithSprites(Asteroid)
+                                            if len(collision) > 0:
+                                                    self.visible=False
+                                                    
     def thrustOn(self, event):
         self.thrust = 1
 
     def thrustOff(self, event):
         self.thrust = 0
+    def left(self, event):
+        self.rx=-5
+        self.rxa=0
+    def right(self, event):
+        self.rx=5
+        self.rxb=0
+    def up(self, event):
+        self.ry=-5
+        self.rya=0
+    def down(self, event):
+        self.ry=5
+        self.ryb=0
+    def stopleft(self, event):
+        self.rxa=2
+    def stopright(self, event):
+        self.rxb=2
+    def stopup(self, event):
+        self.ryb=2
+    def stopdown(self, event):
+        self.rya=2
+    
+        
 
     
 
