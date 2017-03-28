f1e308307e81d995d45759d0672dd67d81be83d3
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-03-28 14:27:28 -0400

    Updated from Brython Server: 3/28/2017 2:27:27 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,9 +1,29 @@
 """
 spaceshooter.py
-Author: <your name here>
+Author: Kyle Postans
 Credit: <list sources used, if any>
 
 Assignment:
 Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+
+SCREEN_WIDTH=2000
+SCREEN_HEIGHT=500
+
+class StarField(Sprite):
+    field=ImageAsset("images/starfield.jpg")
+
+class SpaceShip(Sprite):
+    ship=ImageAsset
+    
+#class Explosions(Sprite):
+    
+#class Scorekeeping(object):   #maybe
+    
+#class Projectiles(Sprite):
+    
+
+    
+class SpaceGame(App):
