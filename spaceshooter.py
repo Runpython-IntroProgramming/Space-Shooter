ce14f9dbc86b0fc057be10c41743d441bd7980d7
samuelpych <samuelpych@hanovernorwichschools.org>
2017-04-04 14:24:29 -0400

    Updated from Brython Server: 4/4/2017 2:24:28 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,9 +1,22 @@
 """
 spaceshooter.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Sam Pych
+Credit: source code for space shhooter
 
 Assignment:
 Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
+
+Your game must include the following:
+
+A fixed star field background.
+At least one player.
+Either multiple playes, or some (in)animate object(s) to avoid.
+Animated rocket thrust for the ship sprites.
+Collisions destroy ships.
+Moving and rotating ships - physics realism at your discretion.
 """
+from ggame import App, Sprite, ImageAsset, Frame
+from ggame import SoundAsset, Sound, TextAsset, Color
+import math
+from time import time
