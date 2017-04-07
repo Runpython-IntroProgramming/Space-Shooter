9de0bf84e56cdf67304d4fa23a42c1ba5572f832
Earljb <earlbarrowes@hanovernorwichschools.org>
2017-04-07 14:34:57 -0400

    Updated from Brython Server: 4/7/2017 2:34:57 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -7,3 +7,29 @@ Assignment:
 Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
+from ggame import App, Sprite, ImageAsset, Frame
+from ggame import SoundAsset, Sound, TextAsset, Color
+import math
+from time import time
+
+class Stars(Sprite):
+
+    asset = ImageAsset("images/starfield.jpg")
+    width = 512
+    height = 512
+
+    def __init__(self, position):
+        super().__init__(Stars.asset, position)
+
+class Sun(Sprite):
+    
+    asset = ImageAsset("images/sun.png")
+    width = 80
+    height = 76
+    
+    def __init__(self, position):
+        super().__init__(Sun.asset, position)
+        self.mass = 30*1000
+        self.fxcenter = 0.5
+        self.fycenter = 0.5
+        self.circularCollisionModel()
