289b68fd2a1e51db1baf9d8b948d17e63343438e
samuelpych <samuelpych@hanovernorwichschools.org>
2017-04-10 14:36:17 -0400

    Updated from Brython Server: 4/10/2017 2:36:16 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -23,21 +23,16 @@ from time import time
 
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
 
-SCREEN_WIDTH = 640
-SCREEN_HEIGHT = 480
 
-"""
 class Stars(Sprite):
-    asset = ImageAsset("images/starfield.jpg",
-        Frame(SCREEN_WIDTH, SCREEN_HEIGHT, 0, 'vertical')   
-"""   
-    """
-    width = SCREEN_WIDTH
-    height = SCREEN_HEIGHT
-    """
+
+    asset = ImageAsset("images/starfield.jpg")
+    width = 512
+    height = 512
+
     def __init__(self, position):
         super().__init__(Stars.asset, position)
-
+        
 class SpaceShip(Sprite):
     asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
         Frame(227,0,292-227,125), 4, 'vertical')
@@ -78,11 +73,11 @@ class SpaceGame(App):
     Tutorial4 space game example.
     """
     def __init__(self, width, height):
-        super().__init__(width, height)
+        super().__init__()
         black = Color(0, 1)
         noline = LineStyle(0, black)
-        bg_asset = RectangleAsset(width, height, noline, black)
-        bg = Sprite(bg_asset, (0,0))
+      
+       
         SpaceShip((100,100))
         
 
@@ -91,5 +86,5 @@ class SpaceGame(App):
             ship.step()
 
 
-myapp = SpaceGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+myapp = SpaceGame(0,0)
 myapp.run()
