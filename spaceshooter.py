f8bee89eb74ed58a3b76aff3712350f7f3582e5e
samuelpych <samuelpych@hanovernorwichschools.org>
2017-04-24 14:28:18 -0400

    Updated from Brython Server: 4/24/2017 2:28:17 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -44,8 +44,8 @@ class SpaceShip(Sprite):
         self.vr = 0.01
         self.thrust = 0
         self.thrustframe = 1
-        SpaceGame.listenKeyEvent("keydown", "space", self.thrustOn)
-        SpaceGame.listenKeyEvent("keyup", "space", self.thrustOff)
+        Spacewar.listenKeyEvent("keydown", "space", self.thrustOn)
+        Spacewar.listenKeyEvent("keyup", "space", self.thrustOff)
         self.fxcenter = self.fycenter = 0.5
 
     def step(self):
@@ -68,23 +68,66 @@ class SpaceShip(Sprite):
 
         
         
-class SpaceGame(App):
-    """
-    Tutorial4 space game example.
-    """
-    def __init__(self, width, height):
-        super().__init__()
-        black = Color(0, 1)
-        noline = LineStyle(0, black)
-      
-       
-        SpaceShip((100,100))
+class Vector:
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
         
+    def mag(self):
+        return math.sqrt(self.x*self.x + self.y*self.y)
+    
+    def unit(self):
+        r = self.mag()
+        if r == 0:
+            return Vector(0,0)
+        else:
+            return Vector(self.x/r, self.y/r)
+class Spacewar(App):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        for x in range(self.width//Stars.width + 1):
+            for y in range(self.height//Stars.height + 1):
+                Stars((x*Stars.width, y*Stars.height))
+        self.listenKeyEvent('keydown', 'space', self.space)
 
+    def space(self, evt):
+        if self.state in ['instructions', 'gameover']:
+            for t in self.tsprites.values():
+                t.visible = False
+            self.state = 'playing'
+            self.Tlast = time()
+            evt.consumed = True
+            self.ship1.newgame()
+            self.ship2.newgame()
+
+        
     def step(self):
-        for ship in self.getSpritesbyClass(SpaceShip):
-            ship.step()
+        explosions = self.getSpritesbyClass(ExplosionSmall)
+        for explosion in explosions:
+            explosion.step()
+        explosions = self.getSpritesbyClass(ExplosionBig)
+        for explosion in explosions:
+            explosion.step()
+        if self.state == 'instructions':
+            self.tsprites['space'].visible = True
+            self.tsprites['left'].visible = True
+            self.tsprites['right'].visible = True
+        elif self.state == 'playing':
+            T = time()
+            dT = T-self.Tlast
+            self.Tlast = T
+            self.ship1.step(T, dT)
+            self.ship2.step(T, dT)
+            if self.ship1.dead or self.ship2.dead:
+                self.state = 'gameover'
+        elif self.state == 'gameover':
+            self.tsprites['space'].visible = True
+            if self.ship1.dead and self.ship2.dead:
+                self.tsprites['tie'].visible = True
+            else:
+                self.tsprites['winner'].visible = True
+                self.tsprites['winner'].x = self.width*3/4-50 if self.ship1.dead else self.width/4-50
 
-
-app = SpaceGame(0,0)
+app = Spacewar(0,0)
 app.run()
