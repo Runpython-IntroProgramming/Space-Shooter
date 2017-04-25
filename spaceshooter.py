39758739e4b74b15bd7796f62926aa526b448e4b
willl4 <williamlaycock@hanovernorwichschools.org>
2017-04-25 14:21:07 -0400

    Updated from Brython Server: 4/25/2017 2:21:07 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -74,7 +74,7 @@ class SpaceShip(Sprite):
             self.setImage(0)
     def thrustOn(self, event):
         self.thrust = 1
-        
+        self.v = 1
         
     def thrustOff(self, event):
         self.thrust = 0
