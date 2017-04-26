265b040334fc0fad53bf7fdcbb11e619ccf391f1
DanielPinigin <danielpinigin@hanovernorwichschools.org>
2017-04-26 13:35:07 -0400

    Updated from Brython Server: 4/26/2017 1:35:07 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -59,6 +59,7 @@ class SpaceGame(App):
         black = Color(0, 1)
         noline = LineStyle(0, black)
         asset = ImageAsset("images/starfield.jpg")
+        bg = Sprite(asset,(0,0))
         SpaceShip((100,100))
         SpaceShip((150,150))
         SpaceShip((200,50))
