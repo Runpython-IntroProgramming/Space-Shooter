eb89fb81ca8a5c96e265c99fc083320c0ba8980c
willl4 <williamlaycock@hanovernorwichschools.org>
2017-05-05 14:02:17 -0400

    Updated from Brython Server: 5/5/2017 2:02:16 PM

--- prob.py
+++ prob.py
@@ -1,1 +1,15 @@
+from random import randint, shuffle
 
+trees = ['M','M','M','O','O','O','O','B','B','B','B','B']
+
+chromosomes = 0
+trogos = 1000
+for i in range(trogos):
+    shuffle(trees)
+    #print(trees)
+    for j in range(9):
+        if trees[j] == 'B' and trees[j+1] == 'B' and trees[j+2] == 'B'and trees[j+3] == 'B':
+            chromosomes+= 1
+            break
+        
+print((trogos+chromosomes)/trogos*100,'%')
