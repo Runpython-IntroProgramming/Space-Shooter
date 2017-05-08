2808536f9e93441d0c14e18370d22465b5099d8c
willl4 <williamlaycock@hanovernorwichschools.org>
2017-05-08 14:27:25 -0400

    Updated from Brython Server: 5/8/2017 2:27:24 PM

--- prob.py
+++ prob.py
@@ -23,18 +23,12 @@ print((trials-failures)/trials*100,'%')"""
 ppl = ['T','T','T','S','S','S','S','S','S','S','S','S','S','S','S']
 
 failures = 0
-trials = 1000
+trials = 10
 for i in range(trials):
     shuffle(ppl)
-    #print(ppl)
-    for j in range(13):
-        if face[j] + face[j+1] + face[j+2] + face[j+3] == 'KKKK':
-            failures += 1
-            break
-        if face[j] + face[j+1] + face[j+2] + face[j+3] == 'QQQQ':
-            failures += 1
-            break
-        if face[j] + face[j+1] + face[j+2] + face[j+3] == 'JJJJ':
+    print(ppl)
+    for j in range(8):
+        if ppl[j] + ppl[j+1] +ppl[j+2] + ppl[j+3]+ppl[j+4] + ppl[j+5] + ppl[j+6] +ppl[j+7] == 'SSTSSTSS':
             failures += 1
             break
         
