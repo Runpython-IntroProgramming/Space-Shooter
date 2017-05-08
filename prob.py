5d9a9d5f2f1e9249a8b414851c54524d594200a6
willl4 <williamlaycock@hanovernorwichschools.org>
2017-05-08 14:08:15 -0400

    Update prob.py

--- prob.py
+++ prob.py
@@ -1,6 +1,6 @@
 from random import randint, shuffle
 
-face = ['K','K','K','K','Q','Q','Q','Q','J','J','J','J']
+"""face = ['K','K','K','K','Q','Q','Q','Q','J','J','J','J']
 
 failures = 0
 trials = 100000
@@ -8,6 +8,26 @@ for i in range(trials):
     shuffle(face)
     #print(face)
     for j in range(8):
+        if face[j] + face[j+1] + face[j+2] + face[j+3] == 'KKKK':
+            failures += 1
+            break
+        if face[j] + face[j+1] + face[j+2] + face[j+3] == 'QQQQ':
+            failures += 1
+            break
+        if face[j] + face[j+1] + face[j+2] + face[j+3] == 'JJJJ':
+            failures += 1
+            break
+        
+print((trials-failures)/trials*100,'%')"""
+
+ppl = ['T','T','T','S','S','S','S','S','S','S','S','S','S','S','S']
+
+failures = 0
+trials = 1000
+for i in range(trials):
+    shuffle(ppl)
+    #print(ppl)
+    for j in range(13):
         if face[j] + face[j+1] + face[j+2] + face[j+3] == 'KKKK':
             failures += 1
             break
