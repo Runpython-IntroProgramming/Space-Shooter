a153679f593fe6b4a2a6d4bd8fe1fe7eb10c3646
Earljb <earlbarrowes@hanovernorwichschools.org>
2017-05-12 14:24:27 -0400

    Updated from Brython Server: 5/12/2017 2:24:26 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -33,13 +33,13 @@ class Sun(Sprite):
         self.fycenter = 0.5
 
 class Ship(Sprite):
-    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png") 
+    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png",
     Frame(227,0,292-227,125), 4, 'vertical')
     def __init__(self, position):
         super().__init__(Ship.asset, position)  
         
-    def __init__(self, app, position, velocity, sun):
-        super().__init__(Ship.asset, app, position, velocity, sun)
+    def __init__(self, app, position):
+        super().__init__(Ship.asset, app, position)
         self.registerKeys(["a", "d", "w", "space"])
         
 class Spacewar(App):
