d630f1f41a466b61211662fd85839da8938ce4e3
Amovi <anthony.movizzo@outlook.com>
2017-05-22 14:55:02 -0400

    Updated from Brython Server: 5/22/2017 2:55:01 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,9 +1,19 @@
-"""
-spaceshooter.py
-Author: <your name here>
-Credit: <list sources used, if any>
+from ggame import App, Sprite, ImageAsset, Frame
+from ggame import SoundAsset, Sound, TextAsset, Color
+import math
+from time import time
 
-Assignment:
-Write and submit a program that implements the spacewar game:
-https://github.com/HHS-IntroProgramming/Spacewar
-"""
+class starfield(Sprite):
+    asset=ImageAsset("Space-Shooter/images/starfield.jpg")
+    width = 512
+    height = 512
+    def __init__(self,position):
+        super().__init__(Stars.asset,position)
+
+class Ship(Sprite):
+    
+    asset = ImageAsset("Space-Shooter/images/four_spaceship_by_albertov_with_thrust.png", 
+        Frame(0,0,86,125), 4, 'vertical')
+
+app = spacegame(0,0)
+app.run()
