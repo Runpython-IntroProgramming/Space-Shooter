71163653afcb25577150812d7f7c3eabf0f8c3f9
Amovi <anthony.movizzo@outlook.com>
2017-05-23 14:55:15 -0400

    Updated from Brython Server: 5/23/2017 2:55:15 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,19 +1,24 @@
-from ggame import App, Sprite, ImageAsset, Frame
-from ggame import SoundAsset, Sound, TextAsset, Color
+from ggame import App, Sprite, ImageAsset, Frame, RectangleAsset
+from ggame import SoundAsset, Sound, TextAsset, Color, LineStyle
 import math
 from time import time
 
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+#background starfield
 class starfield(Sprite):
     asset=ImageAsset("Space-Shooter/images/starfield.jpg")
     width = 512
     height = 512
     def __init__(self,position):
         super().__init__(Stars.asset,position)
-
+#ship
 class Ship(Sprite):
     
     asset = ImageAsset("Space-Shooter/images/four_spaceship_by_albertov_with_thrust.png", 
         Frame(0,0,86,125), 4, 'vertical')
 
-app = spacegame(0,0)
-app.run()
+        
+myapp = App()
+myapp.run()
