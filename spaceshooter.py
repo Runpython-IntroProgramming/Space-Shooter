116a4ad1a2e17917f8e7a3cf190d4210a8f355f0
Amovi <anthony.movizzo@outlook.com>
2017-05-26 14:28:07 -0400

    Updated from Brython Server: 5/26/2017 2:28:06 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -3,7 +3,17 @@
  
 SCREEN_WIDTH = 0
 SCREEN_HEIGHT = 0
-
+#SUNBRUH
+ 
+class sun(Sprite):
+   asset = ImageAsset("images/sun.png")
+   width = 69
+   height = 70
+  
+   def __init__(self, position):
+       super().__init__(sun.asset, position)
+   def step(self):
+       i
 
 #background starfield
 class starfield(Sprite):
