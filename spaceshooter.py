84e3dc9cf5202020d2187f7ce588b048fc2e152d
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-10-17 21:06:26 -0400

    Updated from Brython Server: 10/17/2018 9:06:26 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,9 +1,20 @@
 """
 spaceshooter.py
-Author: <your name here>
+Author: Katie Naughton
 Credit: <list sources used, if any>
 
 Assignment:
 Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
+class SpaceGame(App):
+    def __init__(self):
+        super().__init__()
+        # Background
+        black = Color(0, 1)
+        noline = LineStyle(0, black)
+        bg_asset = RectangleAsset(self.width, self.height, noline, black)
+        bg = Sprite(bg_asset, (0,0))
+myapp = SpaceGame()
+
+myapp.run()
