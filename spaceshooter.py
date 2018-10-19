3b5cb9414deae629f339a135cb98e4e8c0e8caca
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-10-19 12:10:21 -0400

    Updated from Brython Server: 10/19/2018 12:10:21 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,28 +1,21 @@
 """
 spaceshooter.py
 Author: Katie Naughton
-Credit: n/a for now
+Credit: Tutorials
 
 Assignment:
 Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
-class SpaceGame(App):
-    def __init__(self):
-        super().__init__()
-        # Background
-        black = Color(0, 1)
-        noline = LineStyle(0, black)
-        bg_asset = RectangleAsset(self.width, self.height, noline, black)
-        bg = Sprite(bg_asset, (0,0))
-        class SpaceShip(Sprite):
-        
-            asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
-                Frame(227,0,65,125), 4, 'vertical')
 
-            def __init__(self, position):
-                super().__init__(SpaceShip.asset, position)
+    class SpaceShip(Sprite):
+        rocket_asset = ImageAsset("four_spaceship_by_albertov_with_thrust.png")
+        Frame(227,0,65,125), 4, 'vertical')
+    
+    class SpaceGame(App):
+        def __init__(self, position):
+            super().__init__(SpaceShip.asset, position)
 myapp = SpaceGame()
 
 myapp.run()
