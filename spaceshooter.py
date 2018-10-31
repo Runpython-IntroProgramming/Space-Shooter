edfc5b0e675991a3417d1639ef3270b5d07b3c4f
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2018-10-30 20:27:58 -0400

    Updated from Brython Server: 10/30/2018 8:28:00 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -20,6 +20,7 @@ Include these sprites:
     Sun
     Rocket 1
     Rocket 2
+    Bullet Sprite - with noise
     Thruster Sprite - with noise (I think)
     Explosion Sprite - with noise (I think)
 
@@ -35,23 +36,227 @@ Have collisions:
     Basically, make it so that IF a sprite has the same (X, Y) as another sprite, what has been hit explodes.
     
 Start!
-    Structure of code:
-        Code Sprites:
-        Rocket 1
-            Explosions
-            Fake Gravity
-            Movement
-            Collisions
-        Rocket 2
-            Explosions
-            Fake Gravity
-            Movement
-            Collisions
-        Background
-        Sun
-        Code App.Run() stuff
-        Refer to reference if code doesn't work well.
-    
 """
-app = SpaceShootOut()
-app.run()
+SW = 2160
+SH = 1440
+class Sun(Sprite):
+    asset = ImageAsset("images/sun.png")
+    width = 80
+    height = 76
+    def __init__(self, position):
+        super().__init__(Sun.asset, position)
+        self.fxcenter = 0.0
+        self.fycenter = -1.0
+        self.circularCollisionModel()
+class Rocket1(Sprite):
+    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png",
+        Frame(227,0,292-227,125), 4, 'vertical')
+    def __init__(self, position):
+        super().__init__(Rocket1.asset, position)
+        self.vr = 0.00
+        self.thrust = 0
+        self.thrustframe = 1
+        self.VX = 0
+        self.VY = 0   
+        self.vx = 0
+        self.vy = 0
+        self.turn = 0
+        SpaceShootOut.listenKeyEvent("keydown", "up arrow", self.thrustOn)
+        SpaceShootOut.listenKeyEvent("keyup", "up arrow", self.thrustOff)
+#Figure out how to make the space ship move backwards.
+        SpaceShootOut.listenKeyEvent("keydown", "left arrow", self.rotateLeft)
+        SpaceShootOut.listenKeyEvent("keyup", "left arrow", self.lrOff)
+        SpaceShootOut.listenKeyEvent("keydown", "right arrow", self.rotateRight)
+        SpaceShootOut.listenKeyEvent("keyup", "right arrow", self.rrOff)
+        self.fxcenter = self.fycenter = 0.5
+    def step(self):
+        self.rotation += 1.5*self.vr
+        self.move()
+        if self.thrust == 1:
+            self.VX += self.vx
+            self.VY += self.vy
+        if 0 <= self.x <= SW:
+            self.x -= 0.1*self.VX
+        elif self.x < 0:
+            self.x += SW
+            self.x -= 0.1*self.VX
+        else:    
+            self.x -= (0.1*self.VX + SW)
+        if 0 <= self.y <= SH:    
+            self.y -= 0.1*self.VY
+        elif self.y < 0:
+            self.y += SH
+            self.y -= 0.1*self.VY
+        else:
+            self.y -= (0.1*self.VY + SH)
+        if self.thrust == 1:
+            self.setImage(self.thrustframe)
+            self.thrustframe += 1
+            if self.thrustframe == 4:
+                self.thrustframe = 1
+        else:
+            self.setImage(0)
+        collides = self.collidingWithSprites(Rocket2)
+        collides.extend(self.collidingWithSprites(Bullet))
+        if len(collides):
+            if collides[0].visible:
+                collides[0].explode()
+                self.explode()
+    def move(self):
+        self.X = math.sin(self.rotation)
+        self.Y = math.cos(self.rotation)
+        self.vx = self.X/math.sqrt(self.X*self.X + self.Y*self.Y)
+        self.vy = self.Y/math.sqrt(self.X*self.X + self.Y*self.Y)
+    def explode(self):
+        self.visible = False
+        ExplosionBig(self.position)
+        self.waitspawn = 5
+    def thrustOn(self, event):
+        self.thrust = 1
+    def thrustOff(self, event):
+        self.thrust = 0
+    def rotateLeft(self, event):
+        self.vr = 0.05
+    def lrOff(self, event):
+        self.vr = 0
+    def rotateRight(self, event):
+        self.vr = -0.05
+    def rrOff(self, event):
+        self.vr = 0
+class Rocket2(Sprite):
+    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
+        Frame(0,0,86,125), 4, 'vertical')
+    def __init__(self, position):
+        super().__init__(Rocket2.asset, position)
+        self.vr = 0.00
+        self.thrust = 0
+        self.thrustframe = 1
+        self.VX = 0
+        self.VY = 0
+        self.vx = 0
+        self.vy = 0
+        self.turn = 0
+        SpaceShootOut.listenKeyEvent("keydown", "w", self.thrustOn)
+        SpaceShootOut.listenKeyEvent("keyup", "w", self.thrustOff)
+        SpaceShootOut.listenKeyEvent("keydown", "a", self.rotateLeft)
+        SpaceShootOut.listenKeyEvent("keyup", "a", self.lrOff)
+        SpaceShootOut.listenKeyEvent("keydown", "d", self.rotateRight)
+        SpaceShootOut.listenKeyEvent("keyup", "d", self.rrOff)
+        self.fxcenter = self.fycenter = 0.5
+    def step(self):
+        self.rotation += 1.5*self.vr
+        self.move()
+        if self.thrust == 1:
+            self.VX += self.vx
+            self.VY += self.vy
+        if 0 <= self.x <= SW:
+            self.x -= 0.1*self.VX
+        elif self.x < 0:
+            self.x += SW
+            self.x -= 0.1*self.VX
+        else:
+            self.x -= (0.1*self.VX + SW)
+        if 0 <= self.y <= SH:    
+            self.y -= 0.1*self.VY
+        elif self.y < 0:
+            self.y += SH
+            self.y -= 0.1*self.VY
+        else:
+            self.y -= (0.1*self.VY + SH)
+        if self.thrust == 1:
+            self.setImage(self.thrustframe)
+            self.thrustframe += 1
+            if self.thrustframe == 4:
+                self.thrustframe = 1
+        else:
+            self.setImage(0)
+        collides = self.collidingWithSprites(Rocket1)
+        if len(collides):
+            if collides[0].visible:
+                collides[0].explode()
+                self.explode()
+    def move(self):
+        self.X = math.sin(self.rotation)
+        self.Y = math.cos(self.rotation)
+        self.vx = self.X/math.sqrt(self.X*self.X + self.Y*self.Y)
+        self.vy = self.Y/math.sqrt(self.X*self.X + self.Y*self.Y)
+    def explode(self):
+        self.visible = False
+        ExplosionBig(self.position)
+        self.waitspawn = 5
+    def thrustOn(self, event):
+        self.thrust = 1
+    def thrustOff(self, event):
+        self.thrust = 0
+    def rotateLeft(self, event):
+        self.vr = 0.05
+    def lrOff(self,  event):
+        self.vr = 0
+    def rotateRight(self, event):
+        self.vr = -0.05
+    def rrOff(self,  event):
+        self.vr = 0
+class HealthBar:
+    def __init__(self, indicatorasset, initvalue, position, app):
+        self.sprites = [Sprite(indicatorasset, (0,app.height-75)) for i in range(initvalue)]
+        for s in self.sprites:
+            s.scale = 0.4
+        width = self.sprites[0].width
+        if position == 'left':
+            x = 50
+            step = width+5
+        else:
+            x = app.width - 50 - width
+            step = -width-5
+        for s in self.sprites:
+            s.x = x
+            x += step
+        self.restart()
+    def restart(self):
+        for s in self.sprites:
+            s.visible = True
+        self.count = len(self.sprites)
+    def dead(self):
+        return self.count == 0
+    def killone(self):
+        if self.count > 0:
+            self.count -= 1
+            self.sprites[self.count].visible = False
+class ExplosionBig(Sprite):
+    asset = ImageAsset("images/explosion2.png", Frame(0,0,4800/25,195), 25)
+    boomasset = SoundAsset("sounds/explosion2.mp3")
+    def __init__(self, position):
+        super().__init__(ExplosionBig.asset, position)
+        self.image = 0
+        self.center = (0.5, 0.5)
+        self.boom = Sound(ExplosionBig.boomasset)
+        self.boom.play()
+    def step(self):
+        self.setImage(self.image//2)
+        self.image += 1
+        if self.image == 50:
+            self.destroy()
+class SpaceShootOut(App):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        black = Color(0, 1)
+        bg_asset = ImageAsset("images/starfield.jpg")
+        bg = Sprite(bg_asset, (0,0))
+        bg1 = Sprite(bg_asset, (512,512))
+        bg2 = Sprite(bg_asset, (0,512))
+        bg3 = Sprite(bg_asset, (512,0))
+        bg4 = Sprite(bg_asset, (1024,512))
+        bg5 = Sprite(bg_asset, (1024,0))
+        Rocket1((250,250))
+        Rocket2((1000,250))
+        Sun((650,350))
+    def step(self):
+        for Rocket in self.getSpritesbyClass(Rocket1):
+            Rocket.step()
+        for Rocket in self.getSpritesbyClass(Rocket2):
+            Rocket.step()
+        explosions = self.getSpritesbyClass(ExplosionBig)
+        for explosion in explosions:
+            explosion.step()
+myapp = SpaceShootOut(SW, SH)
+myapp.run()
