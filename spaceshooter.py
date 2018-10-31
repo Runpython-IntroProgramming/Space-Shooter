309c6bc7dc391a2675613b15c227926addd42708
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2018-10-31 12:19:26 -0400

    Updated from Brython Server: 10/31/2018 12:19:29 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,7 +1,7 @@
 """
 spaceshooter.py
 Author: <your name here>
-Credit: <list sources used, if any>
+Credit: StackOverflow, ggame documentation, tutorials on the project, and example code.
 
 Assignment:
 Write and submit a program that implements the spacewar game:
