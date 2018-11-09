b3d71d264d7bbe8948784ecdd11c56787baa2e8d
Jacksonlakehhs <42844689+Jacksonlakehhs@users.noreply.github.com>
2018-11-09 12:56:13 -0500

    Updated from Brython Server: 11/9/2018 12:56:13 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -7,3 +7,22 @@ Assignment:
 Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
+from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+
+# add your code here \/  \/  \/
+from ggame import App, Color, LineStyle, Sprite
+from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+
+
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+grey = Color(0xCDC0B0, 1.0)
+firyred = Color(0xFF3030, 1.0)
+purple = Color(0xBF3EFF, 1.0)
+gold = Color(0xFFD700, 1.0)
+white = Color(0xF8F8FF, 1.0)
+violet = Color(0xd147c5, 1.0)
+
+thinline = LineStyle(1, black)
