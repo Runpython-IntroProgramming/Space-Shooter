85205886e63f1f2ae082d0a82640b54bef866e04
NathanSub <42844708+NathanSub@users.noreply.github.com>
2018-11-26 12:35:55 -0500

    Updated from Brython Server: 11/26/2018 12:35:55 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -25,7 +25,7 @@ noline = LineStyle(0, black)
 class Background(App):
     bg_asset = ImageAsset("images/starfield.jpg",
         Frame(227,0,1000,1250), 4, 'vertical')
-    bg = Sprite(bg_asset, (150,150))
+    bg = Sprite(bg_asset, (0,0))
     def __init__(self):
         super().__init__()
 
