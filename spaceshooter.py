9d2268394998295acff5ef50db4037f9065d5655
NathanSub <42844708+NathanSub@users.noreply.github.com>
2018-12-06 13:01:32 -0500

    Updated from Brython Server: 12/6/2018 1:01:31 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -64,7 +64,7 @@ class Rocket(Sprite):
         self.x += self.vx
         self.y += self.vy
         self.rotation += self.vr
-        collision = self.collideWith(sun)
+        
 
         if self.thrust == 5:
             self.setImage(self.thrustframe)
