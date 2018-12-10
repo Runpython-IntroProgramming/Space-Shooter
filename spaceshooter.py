e3823e640cec7bce742298ad9292779ea2a28a5c
NathanSub <42844708+NathanSub@users.noreply.github.com>
2018-12-10 12:54:52 -0500

    Updated from Brython Server: 12/10/2018 12:54:52 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -107,7 +107,7 @@ class SpaceShooter(App):
         for ship in self.getSpritesbyClass(Rocket):
             ship.step()
         if self.rocketship.collidingWith(self.sun):
-            destroy(self):
+            def destroy(self):
                 App._remove(self)
                 self.gfx.destroy()
 
