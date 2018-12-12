24659f4f5408a8154b351bea8018a8a2845137e0
NathanSub <42844708+NathanSub@users.noreply.github.com>
2018-12-12 12:31:11 -0500

    Updated from Brython Server: 12/12/2018 12:31:10 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -125,7 +125,7 @@ class SpaceShooter(App):
                 self.rocketship.destroy()
                 self.rocketship = None
                 
-     def step(self):
+    def step(self):
         explosions = self.getSpritesbyClass(ExplosionSmall)
         for explosion in explosions:
             explosion.step()
