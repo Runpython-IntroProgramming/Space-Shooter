51bff409a6ec67803c1346ad33d99196699f1458
emBrileg08 <46973724+emBrileg08@users.noreply.github.com>
2019-02-27 14:00:00 -0500

    Updated from Brython Server: 2/27/2019 2:00:02 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,9 +1,26 @@
 """
 spaceshooter.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: emBrileg08
+Credit: Example Spaceshooter
 
 Assignment:
 Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
+from ggame import App, Sprite, ImageAsset
+
+class Background(Sprite):
+    def __init__(self):
+        super().__init__()
+    asset=ImageAsset("images/starfield.jpg")
+    width=512
+    height=512
+
+class Spacewar(App):
+    def __init__(self):
+        super().__init__()
+        print(self.height)
+        print(self.width)
+
+myapp=Spacewar()
+myapp.run()
