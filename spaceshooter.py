3ba86c392d62620dec6fd204bb0fecb08baa4968
emBrileg08 <46973724+emBrileg08@users.noreply.github.com>
2019-03-01 14:38:50 -0500

    Updated from Brython Server: 3/1/2019 2:38:48 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -10,18 +10,23 @@ https://github.com/HHS-IntroProgramming/Spacewar
 from ggame import App, Sprite, ImageAsset
 
 class Background(Sprite):
-    def __init__(self):
-        super().__init__()
+    def __init__(self,position):
         asset=ImageAsset("images/starfield.jpg")
-        width=512
-        height=512
+        super().__init__(asset,position)
+    width=512
+    height=512
 
 class Spacewar(App):
     def __init__(self):
         super().__init__()
         z=0
+        a=0
         while z<self.width:
-            Sprite(Background.asset,(z,0))
+            Background((z,0))
+            while a<self.height:
+                Background((z,a))
+                a+=Background.height
+            a=0
             z+=Background.width
     
 myapp=Spacewar()
