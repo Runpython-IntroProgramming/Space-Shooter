d619884de307f1ff827f15053cd5c4c33bdf9a5f
emBrileg08 <46973724+emBrileg08@users.noreply.github.com>
2019-03-01 14:54:34 -0500

    Updated from Brython Server: 3/1/2019 2:54:34 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -15,6 +15,15 @@ class Background(Sprite):
         super().__init__(asset,position)
     width=512
     height=512
+    
+class Spaceship(Sprite):
+    asset=ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
+        Frame((227,0,65,125), 4, 'vertical')
+    def__init__(self,position):
+        super().__init__(asset, position)
+        self.vx=1
+        self.vy=1
+        self.vr=0.01
 
 class Spacewar(App):
     def __init__(self):
