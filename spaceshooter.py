5a676aef221538b729a07bb3924e3c46945ad503
emBrileg08 <46973724+emBrileg08@users.noreply.github.com>
2019-03-02 18:00:58 -0500

    Updated from Brython Server: 3/2/2019 6:00:58 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,13 +1,13 @@
 """
 spaceshooter.py
 Author: emBrileg08
-Credit: Example Spaceshooter code
+Credit: Spacewar Source Code
 
 Assignment:
 Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
-from ggame import App, Sprite, ImageAsset
+from ggame import App, Sprite, ImageAsset, Frame
 
 class Background(Sprite):
     def __init__(self,position):
@@ -17,9 +17,9 @@ class Background(Sprite):
     height=512
     
 class Spaceship(Sprite):
-    asset=ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
-        Frame((227,0,65,125), 4, 'vertical')
-    def__init__(self,position):
+    def __init__(self, position):
+        asset=ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
+            Frame(227,0,65,125), 4, 'vertical')
         super().__init__(asset, position)
         self.vx=1
         self.vy=1
@@ -37,6 +37,7 @@ class Spacewar(App):
                 a+=Background.height
             a=0
             z+=Background.width
+    Spaceship((100,100))
     
 myapp=Spacewar()
 myapp.run()
