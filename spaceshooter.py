40858dbb7c0ebdd6adbde8a73a4a32f44e3eebd2
jaEimele24 <46973541+jaEimele24@users.noreply.github.com>
2019-03-22 14:54:48 -0400

    Updated from Brython Server: 3/22/2019 2:54:47 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -7,3 +7,31 @@ Assignment:
 Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
+from ggame import App, RectangleAsset, ImageAsset, SoundAsset
+from ggame import LineStyle, Color, Sprite, Sound
+import math
+from time import time
+class Background(App):
+    """
+    Tutorial4 space game example.
+    """
+    def __init__(self):
+        super().__init__()
+        # Background
+        black = Color(0, 1)
+        noline = LineStyle(0, black)
+        bg_asset = RectangleAsset(self.width, self.height, noline, black)
+        bg = Sprite(bg_asset, (0,0))
+myapp = Background()
+myapp.run()
+for i in range (1,100):
+    class Stars(App):
+        def __init__(self):
+            super().__init__()  
+            white=Color(0xbbbb00,1)
+            starline=LineStyle(2,white)
+            star_asset =RectangleAsset(10, 10, starline, white)
+            star = Sprite(star_asset, ((20*i),(10*i)))
+    myapp2 = Stars()
+    myapp2.run()
+        
