5e9053d117caec98683678171b6f2e80a8811ed5
jaEimele24 <46973541+jaEimele24@users.noreply.github.com>
2019-03-25 14:48:24 -0400

    Updated from Brython Server: 3/25/2019 2:48:23 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -2,36 +2,31 @@
 spaceshooter.py
 Author: <your name here>
 Credit: <list sources used, if any>
-
 Assignment:
 Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
 from ggame import App, RectangleAsset, ImageAsset, SoundAsset
-from ggame import LineStyle, Color, Sprite, Sound
+from ggame import LineStyle, Color, Sprite, Sound, Frame
 import math, random
 from time import time
 class Background(App):
-    """
-    Tutorial4 space game example.
-    """
     def __init__(self):
         super().__init__()
-        # Background
         black = Color(0, 1)
         noline = LineStyle(0, black)
         bg_asset = RectangleAsset(self.width, self.height, noline, black)
         bg = Sprite(bg_asset, (0,0))
-myapp = Background()
-myapp.run()
-for i in range (1,100):
-    class Stars(App):
-        def __init__(self):
-            super().__init__()  
+        for i in range (1,100):
             white=Color(0xbbbb00,1)
             starline=LineStyle(2,white)
             star_asset =RectangleAsset(10, 10, starline, white)
             star = Sprite(star_asset, ((random.randint(0,1000)),(random.randint(0,1000))))
-    myapp2 = Stars()
-    myapp2.run()
-        
+class SpaceShip(Sprite):
+    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png",
+        Frame(227,0,65,125), 4, 'vertical')
+    def __init__(self, position):
+        super().__init__(SpaceShip.asset, position)
+myapp = Background()
+myapp.run()
+
