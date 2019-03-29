f48097f6978fe7f6f6b7906374ff312daf99bba1
anEndrel23 Hhs <44811950+anEndrel23@users.noreply.github.com>
2019-03-29 14:29:25 -0400

    Updated from Brython Server: 3/29/2019 2:29:15 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -8,98 +8,27 @@ Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
 
-from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame, CircleAsset
+from ggame import SoundAsset, Sound, TextAsset, Color
+import math
+from time import time
 
-class SpaceShip(Sprite):
-    """
-    Animated space ship
-    """
-    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
-        Frame(227,0,65,125), 4, 'vertical')
-
+class Space(Sprite):
+    asset = ImageAsset("images/starfield.jpg")
+    width = 512
+    height = 512
     def __init__(self, position):
-        super().__init__(SpaceShip.asset, position)
-        self.vx = 1
-        self.vy = 1
-        self.vr = 0.01
-        self.thrust = 0
-        self.thrustframe = 1
-        SpaceGame.listenKeyEvent("keydown", "space", self.thrustOn)
-        SpaceGame.listenKeyEvent("keyup", "space", self.thrustOff)
-        self.fxcenter = self.fycenter = 0.5
-
-    def step(self):
-        self.x += self.vx
-        self.y += self.vy
-        self.rotation += self.vr
-        # manage thrust animation
-        if self.thrust == 1:
-            self.setImage(self.thrustframe)
-            self.thrustframe += 1
-            if self.thrustframe == 4:
-                self.thrustframe = 1
-        else:
-            self.setImage(0)
-
-    def thrustOn(self, event):
-        self.thrust = 1
+        super().__init__(Space.asset, position)
         
-    def thrustOff(self, event):
-        self.thrust = 0
+class Sun(Sprite):
+    
+    width = 80
+    height = 76
+    asset = ImageAsset("images/sun.png", Frame(0, 0, width, height))
+    
+    def __init__(self, position):
+        super().__init__(Sun.asset, position, CircleAsset(32))
 
-
-class SpaceGame(App):
-    """
-    Tutorial4 space game example.
-    """
-    def __init__(self):
-        super().__init__()
-        # Background
-        black = Color(0, 1)
-        noline = LineStyle(0, black)
-        bg_asset = RectangleAsset(self.width, self.height, noline, black)
-        bg = Sprite(bg_asset, (0,0))
-        SpaceShip((100,100))
-        SpaceShip((150,150))
-        SpaceShip((200,50))
-
-    def step(self):
-        for ship in self.getSpritesbyClass(SpaceShip):
-            ship.step()
-
-        
-myapp = SpaceGame()
-myapp.run()
-
-"""
-from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
-
-red = Color(0xff0000, 1.0)
-green = Color(0x00ff00, 1.0)
-blue = Color(0x0000ff, 1.0)
-black = Color(0x000000, 1.0)
-yellow = Color(0xFFFF00, 1.0)
-skyblue = Color(0x87CEEB, 1.0)
-grey = Color(0x696969, 1.0)
-brick = Color(0xA52A2A, 1.0)
-khaki = Color(0xF0E68C, 1.0)
-
-
-thinline = LineStyle(1, black)
-
-space = RectangleAsset(1500, 1000, thinline, black)
-
-star = PolygonAsset([(0,10), (14,0), (28,10), (22,30), (6, 30)], thinline, yellow)
-
-
-
-
-Sprite(space, (0, 0))
-
-Sprite(star, (200, 200))
-
+Space((0,0))
 myapp = App()
-myapp.width = 1600
-myapp.height = 900
 myapp.run()
-"""
