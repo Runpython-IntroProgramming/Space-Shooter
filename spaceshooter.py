6a278193e1099eb7aa0ce473e88f22d896379c25
sth23 <47009471+sth23@users.noreply.github.com>
2019-04-10 17:06:12 -0400

    Updated from Brython Server: 4/10/2019 5:06:15 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -15,7 +15,8 @@ class Bullet(Sprite):
     
     red = Color(0xff0000, 1.0)
     noline = LineStyle(0, red)
-    asset = CircleAsset(5, noline, red)
+    #asset = CircleAsset(5, noline, red)
+    asset = ImageAsset("image/blast.png")
     
     def __init__(self, position, direction):
         super().__init__(Bullet.asset, [position[0] - 50 * math.sin(direction), position[1] - 50 * math.cos(direction)])
