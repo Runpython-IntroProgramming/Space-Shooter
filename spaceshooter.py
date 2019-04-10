313676397195ab4c5365e02ec65e7004d8e72b4b
sth23 <47009471+sth23@users.noreply.github.com>
2019-04-10 17:03:09 -0400

    Updated from Brython Server: 4/10/2019 5:03:12 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -142,13 +142,13 @@ class SpaceGame(App):
         noline = LineStyle(0, black)
         #bg_asset = RectangleAsset(self.width, self.height, noline, black)
         #bg = Sprite(bg_asset, (0,0))
-        starfield_asset = ImageAsset("images/starfield.jpg")
-        starfield_sprite = Sprite(starfield_asset, (0,0))
+        #starfield_asset = ImageAsset("images/starfield.jpg")
+        #starfield_sprite = Sprite(starfield_asset, (0,0))
         # Scale the sprite according to size of screen
-        if self.width > self.height:
-            starfield_sprite.scale = self.width / starfield_sprite.width
-        else:
-            starfield_sprite.scale = self.height / starfield_sprite.height
+        #if self.width > self.height:
+        #    starfield_sprite.scale = self.width / starfield_sprite.width
+        #else:
+        #    starfield_sprite.scale = self.height / starfield_sprite.height
         
         sun_asset = ImageAsset("images/sun.jpg")
         sun_sprite = Sprite(sun_asset, (self.width / 2, self.height / 2))
