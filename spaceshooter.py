e3bc0c29a999cf749d39578400f8b27b88104e08
sth23 <47009471+sth23@users.noreply.github.com>
2019-04-12 14:32:26 -0400

    Updated from Brython Server: 4/12/2019 2:32:28 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -70,7 +70,7 @@ class SpaceShip(Sprite):
         
     def thrustOn(self, event):
         self.thrust = 1
-        speed_limit = (self.vx^2 + self.vy^2)^0.5
+        speed_limit = (self.vx**2+ self.vy**2)**0.5
         if speed_limit < 25:
             self.vx += -(math.sin(self.rotation)) *0.05
             self.vy += -(math.cos(self.rotation)) * 0.05
