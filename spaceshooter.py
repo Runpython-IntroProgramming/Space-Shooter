3705bb3202df4ba0b800369d6d5a4d653b3e856a
sth23 <47009471+sth23@users.noreply.github.com>
2019-04-25 14:35:32 -0400

    Updated from Brython Server: 4/25/2019 2:35:32 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -130,7 +130,7 @@ class EnemyShip(Sprite):
         self.vr = 0.00
         
         # Spaceship thrust on/off
-        self.thrust = 1
+        self.thrust = 0
         self.thrustframe = 1
         self.fxcenter = self.fycenter = 0.45
         
