91e56231a7b687f4e33ca4b05f8f8fa6c4d3b425
sth23 <47009471+sth23@users.noreply.github.com>
2019-04-26 12:00:06 -0400

    Updated from Brython Server: 4/26/2019 12:00:06 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -253,7 +253,6 @@ class SpaceGame(App):
             bullet.step()
             if bullet.x < -10 or bullet.x > self.width + 10 or bullet.y < -10 or bullet.y > self.height + 10:
                 bullet.destroy()
-            bullet.collisions()
 
 myapp = SpaceGame()
 myapp.run()
