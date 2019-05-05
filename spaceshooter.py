23a745a2da506311a7ebaa64d5201219029dbfbc
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-05 17:38:19 -0400

    Updated from Brython Server: 5/5/2019 5:38:19 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -308,6 +308,7 @@ class SpaceGame(App):
             # Check to see if ship has collided with anything
             if ship.collision():
                 EnemyShip((random.randint(0,self.width),random.randint(0,self.height)))
+                EnemyShip((random.randint(0,self.width),random.randint(0,self.height)))
                 
         for bullet in self.getSpritesbyClass(Bullet):
             bullet.step()
