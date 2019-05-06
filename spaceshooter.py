e4c7637e6070b59afb881cb2f9195e191147a4fe
maBottnn14 Hhs <46973490+MaBottnn14@users.noreply.github.com>
2019-05-06 14:45:43 -0400

    Updated from Brython Server: 5/6/2019 2:45:40 PM

--- spaceshooter.py
+++ spaceshooter.py
@@ -8,14 +8,13 @@ Write and submit a program that implements the spacewar game:
 https://github.com/HHS-IntroProgramming/Spacewar
 """
 from ggame import App, Sprite, ImageAsset, Frame, CircleAsset
-from ggame import SoundAsset, Sound, TextAsset, Color
+from ggame import SoundAsset
 import math
 from time import time
-from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
-
 
 myapp = App()
-#StarField:
+
+#Starfield
 class Stars(Sprite):
     asset = ImageAsset("images/starfield.jpg")
     width = 512
@@ -23,28 +22,76 @@ class Stars(Sprite):
     
     def __init__(self, position):
         super().__init__(Stars.asset, position)
-
 #Monster
 class monster(Sprite):
-    op = ImageAsset("images/OGChungus.png")
+    op = ImageAsset("images/Alienblue.gif")
     width = 50
     height = 50
     
     def __init__(self, position):
         super().__init__(monster.op, position)
-        self.scale = 1
+        self.scale = 0.36
+#Space ship
+class SpaceShip(Sprite):
 
+    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
+        Frame(227,0,292-227,125), 4, 'vertical')
 
-   
-
-
-monster((1000,0))
+    def __init__(self, position):
+        super().__init__(SpaceShip.asset, position)
+        self.vx = 1
+        self.vy = 1
+        self.vr = 0.02
+        self.thrust = 0
+        self.thrustframe = 1
+        self.fxcenter = self.fycenter = 0.5
+    
+    def step(self, monster):
+        if self.thrust == 1:
+            self.setImage(self.thrustframe)
+            self.thrustframe += 1
+            if self.thrustframe == 4:
+                self.thrustframe = 1
+            self.x += -sin(self.rotation)
+            self.y += -cos(self.rotation)
+            if self.collidingWith(monster):
+                self.destroy()
+        else:
+            self.setImage(0)
+    def thrustOn(self, event):
+        self.thrust = 1
+        
+    def thrustOff(self, event):
+        self.thrust = 0
+    
+    def TurnLOn(self, event):
+        self.rotation += self.vr
+        
+    def TurnROn(self, event):
+        self.rotation -= self.vr
+    
 Stars((512,0))
 Stars((0,0))
 Stars((1024,0))
-Stars((0, 512))
-Stars((512, 512))
-Stars((1024, 512))
+Stars((0,512))
+Stars((512,512))
+Stars((1024,512))
+SpaceShip((400,450))        
+monster((500,200))
+class SpaceGame(App):
 
-myapp = App()
+    def __init__(self):
+        super().__init__()
+        hi = ImageAsset("images/Alienblue.gif")
+        hello = ImageAsset("images/Alienblue.gif")
+        
+        
+    def step(self):
+        for ship in self.getSpritesbyClass(SpaceShip):
+            ship.step(self.hello_sprite)
+
+myapp.listenKeyEvent('keydown', 'w', myapp.ship.thrustOn)
+myapp.listenKeyEvent('keyup', '1', myapp.ship.thrustOff)
+myapp.listenKeyEvent('keydown', 'a', myapp.ship.TurnLOn)
+myapp.listenKeyEvent('keydown', 'd', myapp.ship.TurnROn)
 myapp.run()
