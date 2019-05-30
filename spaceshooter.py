cfcbffac65064467245b69250b742c8150000bd4
maCucyrt07 <46973482+maCucyrt07@users.noreply.github.com>
2019-05-30 09:29:33 -0400

    Updated from Brython Server: 5/30/2019 9:29:32 AM

--- spaceshooter.py
+++ spaceshooter.py
@@ -1,15 +1,6 @@
 """
 spaceshooter.py
 Author: MaCucyrt07
-Credit: <list sources used, if any>
-
-Assignment:
-Write and submit a program that implements the spacewar game:
-https://github.com/HHS-IntroProgramming/Spacewar
-"""
-"""
-spaceshooter.py
-Author: MaCucyrt07
 Credit: Katie, Ella, Andrew
 
 Assignment:
